5385628e6374752a58a76c80a31e1c3224c52ba0
Max Rozvora <mr@exdst.com.ua>
2021-02-25 13:16:00 +0200

    toggle navigation menu

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/app.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/app.js
@@ -3,5 +3,18 @@ window.jQuery = $;
 window.$ = $;
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Custom JS
+  // Toogle navigation menu
+
+  const showMenu = () => {
+    $(".sidebar").addClass("sidebar_active");
+    $(".sidebar-overlay").addClass("sidebar-overlay_active");
+  };
+  const hideMenu = () => {
+    $(".sidebar").removeClass("sidebar_active");
+    $(".sidebar-overlay").removeClass("sidebar-overlay_active");
+  };
+
+  $(".header-navbar__btn-open").on("click", showMenu);
+  $(".sidebar-overlay").on("click", hideMenu);
+  $(".sidebar__close-btn").on("click", hideMenu);
 });
