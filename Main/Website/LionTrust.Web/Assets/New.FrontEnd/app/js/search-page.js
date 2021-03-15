48bcdcd3f63b1bd0ca5411a1c7f5eeb102f331c6
Max Rozvora <mr@exdst.com.ua>
2021-03-15 17:12:18 +0200

    create query string request

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/search-page.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/search-page.js
@@ -1,21 +1,29 @@
 import Vue from "vue/dist/vue.common.prod";
 
-console.log("Vue", Vue);
 export default () => {
-  const host = "http://localhost:3004/results";
-  const showPerPage = 2;
+  const host = "http://localhost:3004/results?";
+  const amountResults = 123;
+  const showPerPage = 10;
   new Vue({
     el: "#result-list-app",
     data: {
-      inputText: "",
       results: [],
+      searchParams: {
+        queryText: "",
+        type: "",
+        page: 1,
+      },
       allResults: true,
       labels: [
-        { title: "Funds", checked: false },
-        { title: "Funds Managers", checked: false },
-        { title: "Articles", checked: false },
-        { title: "Documents", checked: false },
-        { title: "Pages", checked: false },
+        { title: "Funds", categoryName: "Funds", checked: false },
+        {
+          title: "Funds Managers",
+          categoryName: "Funds Managers",
+          checked: false,
+        },
+        { title: "Articles", categoryName: "Article", checked: false },
+        { title: "Documents", categoryName: "Document", checked: false },
+        { title: "Pages", categoryName: "Page", checked: false },
       ],
     },
     computed: {
@@ -37,16 +45,27 @@ export default () => {
 
         return urlParams;
       },
+      generateSearchParams() {
+        let query = "";
+        for (const [key, value] of Object.entries(this.searchParams)) {
+          if (value) query += `${key}=${value}&`;
+        }
+        console.log('query',query);
+        return query;
+      },
     },
     methods: {
       labelClick() {
         this.allResults = false;
       },
       submit() {
-        console.log(this.inputText);
+        this.searchParams.type = '',
+        this.searchParams.page = 1
+        this.serchRequest()
       },
-      serchRequest() {
-        $.ajax(host)
+      serchRequest(params = this.generateSearchParams) {
+        this.changeUrl(params)
+        $.ajax(host + params)
           .done((res) => {
             this.results = res;
           })
@@ -54,23 +73,36 @@ export default () => {
             throw new Error("Data does not fetch " + e);
           });
       },
+      changeUrl(searchParams) {
+        console.log('changeUrl');
+        window.history.pushState(
+          { page: "search-page" },
+          "search",
+          `${window.location.href.split("?")[0]}?${searchParams}`
+        );
+      },
     },
     watch: {
       allResults() {
         this.labels.forEach((label) => (label.checked = false));
-        window.history.pushState(
-          { page: "another" },
-          "value",
-          "http://localhost:3000/search-page.html"
-        );
+      },
+      labels: {
+        handler(val) {
+          const filters = val.filter((item) => item.checked);
+          let type = "";
+          filters.forEach((item, i) => {
+            if (filters.length - 1 !== i) type += `${item.categoryName},`;
+            else type += `${item.categoryName}`;
+          });
+          console.log('type',type);
+          this.searchParams.type = type
+          this.serchRequest();
+        },
+        deep: true,
       },
     },
     mounted() {
-      this.serchRequest();
-      console.log("works");
-      console.log("this.getReqValue", this.getReqValue);
-
-      this.inputText = this.getReqValue.queryText;
+      this.serchRequest(this.searchParams.queryText);
     },
   });
 };
