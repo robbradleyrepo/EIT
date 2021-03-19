eebc1463a857f4571dc6804d599ef5aa7eb49295
Max Rozvora <mr@exdst.com.ua>
2021-03-19 17:06:37 +0200

    create Investment-Process

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/app.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/app.js
@@ -5,9 +5,11 @@ window.$ = $;
 import onboardingOverlay from "./componets/onboarding";
 import searchOvarlay from "./componets/searchOvarlay";
 import sidebarNav from "./componets/sidebarNav";
+import investmentCard from "./componets/investmentCard";
 
 document.addEventListener("DOMContentLoaded", () => {
   onboardingOverlay();
   sidebarNav();
   searchOvarlay();
+  investmentCard()
 });
---
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/investmentCard.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+
+export default () => {    
+    const swiper = new Swiper('#investment-slider', {
+        slidesPerView: 1.2,
+        spaceBetween: 32,
+        slidesOffsetAfter: 30,
+        breakpointsInverse: true,
+        breakpoints: {
+            576: {
+                slidesPerView: 1.5,
+            },
+            768: {
+                slidesPerView: 2.2,
+            },
+            992: {
+                slidesPerView: 2.7,
+            },
+            1200: {
+                slidesPerView: 3.5,
+            },
+            2560: {
+                slidesPerView: 4.5,
+            }
+        }
+      });
+}
