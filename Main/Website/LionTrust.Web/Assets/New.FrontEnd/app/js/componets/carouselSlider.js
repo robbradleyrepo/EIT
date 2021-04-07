02c44b17235fa5af598b246d1d6f082e4b496bb4
max <rozvora.max@gmail.com>
2021-04-07 11:21:33 +0300

    fix caoruse on mobile

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
@@ -1,40 +1,35 @@
-
-
-
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // core version + navigation, pagination modules:
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
 
 // configure Swiper to use modules
 SwiperCore.use([Navigation, Pagination]);
 
 export default () => {
-    
-    const swiper = new Swiper('.swiper-container-carousel', {
+  const swiper = new Swiper(".swiper-container-carousel", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    breakpointsInverse: true,
+    slidesPerView: "auto",
+    spaceBetween: 5,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 2,
+      slideShadows: true,
+    },
 
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        spaceBetween: 5,
-        coverflowEffect: {
-            rotate: 10,
-            stretch: 0,
-            depth: 300,
-            modifier: 2,
-            slideShadows: true,
-        },
-            
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-}
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
