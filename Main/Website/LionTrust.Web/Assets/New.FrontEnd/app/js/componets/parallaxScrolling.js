4f902b83920476b3a90fa75a9ddd343f94a1609b
jason.cox <jason.cox@sagittarius-marketing.com>
2021-04-14 12:50:42 +0000

    Merged in feature/adding_docker (pull request #2)
    Feature/adding docker
    
    * set background for home page
    
    * Merge remote-tracking branch 'origin/master'
    
    * fix homepage bugs, add parallax scrolling
    
    * rollback to bootstrap 4
    
    * Adding files to run in Docker
    
    * Use fixed names for containers. Update readme
    
    * Merged master into feature/adding_docker
    
    Approved-by: Peter Clisby

---
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/parallaxScrolling.js
@@ -0,0 +1,34 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to(".paralax-circle__item", {
+    scrollTrigger: {
+      trigger: ".paralax-circle__item",
+      start: "top center",
+      // markers: true,
+      scrub: true,
+    },
+    y: -500,
+  });
+
+  gsap.to(".shevron-bg", {
+    scrollTrigger: {
+      trigger: ".shevron-bg",
+      start: "top center",
+      // markers: true,
+      scrub: true,
+    },
+    x: 500,
+  });
+
+  gsap.to(".lead-banner__bg", {
+    scrollTrigger: {
+      trigger: ".lead-banner__bg",
+      start: "top center",
+      scrub: true,
+    },
+    y: -100,
+  });
+};
