d9c6c1d6f7188f7d6ba6224773a67cf77ab5fb13
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-04-16 15:40:02 +0100

    adding newsletter promo component

---
+++ src/Feature/Newsletter/tests/LionTrust.Feature.NewsletterTests/Promo/NewsletterPromoControllerShould.cs
@@ -0,0 +1,29 @@
+﻿using LionTrust.Feature.Newsletter.Promo;
+using LionTrust.Foundation.Content.Repositories;
+using Moq;
+using NUnit.Framework;
+
+namespace LionTrust.Feature.NewsletterTests.Promo
+{
+    [TestFixture]
+    public class NewsletterPromoControllerShould
+    {
+        private Mock<IRenderingRepository> repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            repository = new Mock<IRenderingRepository>();
+        }
+
+        [Test]
+        public void NotThrowAnExceptionWhenWeTryToRender()
+        {
+            // Arrange
+            var target = new NewsletterPromoController(repository.Object);
+
+            // Act + Assert
+            Assert.DoesNotThrow(() => target.Render());
+        }
+    }
+}
---
+++ src/Feature/Newsletter/tests/LionTrust.Feature.NewsletterTests/Promo/NewsletterPromoViewModelShould.cs
@@ -0,0 +1,45 @@
+﻿using LionTrust.Feature.Newsletter.Promo;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace LionTrust.Feature.NewsletterTests.Promo
+{
+    [TestFixture]
+    public class NewsletterPromoViewModelShould
+    {
+        private Mock<INewsletterPromoModel> data;
+
+        [SetUp]
+        public void Setup()
+        {
+            data = new Mock<INewsletterPromoModel>();
+        }
+
+        [Test]
+        public void ExtractTheGoalIdFromTheGoal()
+        {
+            // Arrange
+            data.SetupGet(d => d.Goal).Returns("B81359EA-DFDA-4A80-BDAC-6F55B9F2AF64");
+            var target = new NewsletterPromoViewModel(data.Object);
+
+            // Act
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, target.GoalId);
+        }
+
+        [Test]
+        public void ExtractTheBackgroundImageUrlFromTheBackgroundImage()
+        {
+            // Arrange
+            data.SetupGet(d => d.BackgroundImage).Returns(new Glass.Mapper.Sc.Fields.Image { Src = "http://www.google.co.uk/imge.png" });
+            var target = new NewsletterPromoViewModel(data.Object);
+
+            // Act
+
+            // Assert
+            Assert.IsNotEmpty(target.BackgroundImageUrl);
+        }
+    }
+}
