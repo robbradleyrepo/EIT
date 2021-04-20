f76e428fdbb2d0c8fa5f9fc94f26bfa27cea588d
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-20 21:58:58 +0300

    LDC-581 Image CTA Promo component implementation

---
+++ src/Feature/Promo/website/Models/IImageCTAPromo.cs
@@ -0,0 +1,28 @@
+﻿namespace LionTrust.Feature.Promo.Models
+{
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+
+    public interface IImageCTAPromo : IPromoGlassBase
+    {
+        [SitecoreField(Constants.ImageCTAPromo.Image_FieldId, SitecoreFieldType.Image, "Image CTA Promo")]
+        Image Image { get; set; }
+
+        [SitecoreField(Constants.ImageCTAPromo.Title_FieldId, SitecoreFieldType.SingleLineText, "Image CTA Promo")]
+        string Title { get; set; }
+
+        [SitecoreField(Constants.ImageCTAPromo.Subtitle_FieldId, SitecoreFieldType.SingleLineText, "Image CTA Promo")]
+        string Subtitle { get; set; }
+
+        [SitecoreField(Constants.ImageCTAPromo.Body_FieldId, SitecoreFieldType.MultiLineText, "Image CTA Promo")]
+        string Body { get; set; }
+
+        [SitecoreField(Constants.ImageCTAPromo.CTALink_FieldId, SitecoreFieldType.GeneralLink, "Image CTA Promo")]
+        Link CTALink { get; set; }
+
+        [SitecoreField(Constants.ImageCTAPromo.CTALabel_FieldId, SitecoreFieldType.SingleLineText, "Image CTA Promo")]
+        string CTALabel { get; set; }
+
+    }
+}
