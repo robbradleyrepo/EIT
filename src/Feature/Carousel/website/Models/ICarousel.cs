bb2e6f798f8c02472d9bc18d71307ae8b37395be
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-21 20:15:41 +0300

    LDC-182 Homepage Carousel initial implementation

---
+++ src/Feature/Carousel/website/Models/ICarousel.cs
@@ -0,0 +1,32 @@
+﻿namespace LionTrust.Feature.Carousel.Models
+{
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+    using LionTrust.Feature.Carousel;
+    using System;
+
+    public interface ICarousel : ICarouselGlassBase
+    {
+        [SitecoreField(Constants.CarouselCard.AuthorImage_FieldId, SitecoreFieldType.DropTree, "Carousel Card")]
+        Image AuthorImage { get; set; }
+
+        [SitecoreField(Constants.CarouselCard.AuthorName_FieldId, SitecoreFieldType.DropTree, "Carousel Card")]
+        string AuthorName { get; set; }
+
+        [SitecoreField(Constants.CarouselCard.Heading_FieldId, SitecoreFieldType.SingleLineText, "Carousel Card")]
+        string Heading { get; set; }        
+
+        [SitecoreField(Constants.CarouselCard.CTALink_FieldId, SitecoreFieldType.GeneralLink, "Carousel Card")]
+        Link CTALink { get; set; }
+
+        [SitecoreField(Constants.CarouselCard.CTAGoal_FieldId, SitecoreFieldType.Droplink, "Carousel Card")]
+        Guid CTAGoal { get; set; }
+
+        [SitecoreField(Constants.CarouselCard.CTALabel_FieldId, SitecoreFieldType.SingleLineText, "Carousel Card")]
+        string CTALabel { get; set; }
+
+        [SitecoreField(Constants.CarouselCard.Image_FieldId, SitecoreFieldType.Image, "Carousel Card")]
+        Image Image { get; set; }
+    }
+}
---
+++ src/Feature/Carousel/website/Models/IHomepageCarousel.cs
@@ -0,0 +1,42 @@
+﻿namespace LionTrust.Feature.Carousel.Models
+{
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+    using LionTrust.Feature.Carousel;
+    using System;
+    using System.Collections.Generic;
+
+    public interface IHomepageCarousel : ICarouselGlassBase
+    {
+        [SitecoreChildren]
+        IEnumerable<ICarousel> CarouselCards { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.Heading_FieldId, SitecoreFieldType.SingleLineText, "Homepage Carousel")]
+        string Heading { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.Body_FieldId, SitecoreFieldType.MultiLineText, "Homepage Carousel")]
+        string Body { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.PrimaryCTALink_FieldId, SitecoreFieldType.GeneralLink, "Homepage Carousel")]
+        Link PrimaryCTALink { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.PrimaryCTAGoal_FieldId, SitecoreFieldType.Droplink, "Homepage Carousel")]
+        Guid PrimaryCTAGoal { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.PrimaryCTALabel_FieldId, SitecoreFieldType.SingleLineText, "Homepage Carousel")]
+        string PrimaryCTALabel { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.SecondaryCTALink_FieldId, SitecoreFieldType.GeneralLink, "Homepage Carousel")]
+        Link SecondaryCTALink { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.SecondaryCTAGoal_FieldId, SitecoreFieldType.Droplink, "Homepage Carousel")]
+        Guid SecondaryCTAGoal { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.SecondaryCTALabel_FieldId, SitecoreFieldType.SingleLineText, "Homepage Carousel")]
+        string SecondaryCTALabel { get; set; }
+
+        [SitecoreField(Constants.HomePageCarousel.NumberOfItems_FieldId, SitecoreFieldType.Integer, "Homepage Carousel")]
+        int NumberOfItems { get; set; }
+    }
+}
