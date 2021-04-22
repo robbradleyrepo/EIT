94b19aca2f250e2d420d972e676c383159ce2ce9
max <rozvora.max@gmail.com>
2021-04-22 15:25:47 +0300

    fix carouse at header

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
@@ -10,20 +10,57 @@ export default () => {
   const swiper = new Swiper(".swiper-container-carousel", {
     effect: "coverflow",
     // grabCursor: true,
+    slidesPerView: "auto",
+    watchSlidesVisibility: true,
+    loop: false,
     centeredSlides: true,
     breakpointsInverse: true,
-    slidesPerView: "auto",
     longSwipes: false,
     setWrapperSize: true,
     spaceBetween: 5,
     coverflowEffect: {
       rotate: 0,
-      stretch: 125,
-      depth: 300,
-      modifier: 2,
+      stretch: 225,
+      depth: 200,
+      modifier: 1.8,
       slideShadows: true,
     },
-
+    breakpoints: {
+      576: {},
+      768: {
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 100,
+          depth: 600,
+          modifier: 1.1,
+          slideShadows: true,
+        },
+      },
+      992: {
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 225,
+          depth: 500,
+          modifier: 1.2,
+          slideShadows: true,
+        },
+      },
+      1200: {
+        coverflowEffect: {
+          modifier: 1.8,
+        },
+      },
+      1800: {
+        coverflowEffect: {
+          modifier: 1.8,
+        },
+      },
+      2560: {
+        stretch: 225,
+        depth: 300,
+        modifier: 1.5,
+      },
+    },
     loop: true,
     pagination: {
       el: ".swiper-pagination",
