b8f89deb3fabd084bb719e4d41c8e33b76ef01a1
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-26 16:37:12 +0300

    Clean from carousel wrong classes, populating the views with glass references by sitecore

--- src/Feature/Promo/website/Controllers/PromoController.cs
+++ src/Feature/Promo/website/Controllers/PromoController.cs
@@ -3,21 +3,21 @@
     using System.Web.Mvc;
 
     using Glass.Mapper.Sc.Web.Mvc;
-    using LionTrust.Feature.Carousel.Models;
+    using LionTrust.Feature.Promo.Models;
     using LionTrust.Foundation.Indexing.Repositories;
     using Sitecore.Mvc.Controllers;
     using Sitecore.Mvc.Presentation;
 
-    public class CarouselController : SitecoreController
+    public class PromoController : SitecoreController
     {
         private readonly ISearchRepository _searchRepository;
         private readonly IMvcContext _mvcContext;
 
-        public CarouselController(IMvcContext mvcContext) : this(new SearchRepository(RenderingContext.Current.ContextItem), mvcContext)
+        public PromoController(IMvcContext mvcContext) : this(new SearchRepository(RenderingContext.Current.ContextItem), mvcContext)
         {
         }
 
-        public CarouselController(ISearchRepository searchRepository, IMvcContext mvcContext)
+        public PromoController(ISearchRepository searchRepository, IMvcContext mvcContext)
         {
             this._searchRepository = searchRepository;
             this._mvcContext = mvcContext;
@@ -26,6 +26,11 @@
         public ActionResult ArticleScroller()
         {
             var articleScrollerViewModel = new ArticleScrollerViewModel();
+            articleScrollerViewModel.ArticleScroller = _mvcContext.GetDataSourceItem<IArticleScroller>();
+            if (articleScrollerViewModel.ArticleScroller.SelectedArticles != null)
+            {
+                articleScrollerViewModel.ArticleList = articleScrollerViewModel.ArticleScroller.SelectedArticles;
+            }
 
             return View("~/Views/Carousel/ArticleScroller.cshtml", articleScrollerViewModel);
         }
