f70877576399a6607b0ba2ad32025176643ace57
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-04-26 12:26:52 +0100

    adding start of fund scroller

---
+++ src/Feature/Carousel/website/Controllers/FundScrollerController.cs
@@ -0,0 +1,30 @@
+﻿namespace LionTrust.Feature.Carousel.Controllers
+{
+    using Glass.Mapper.Sc;
+    using Glass.Mapper.Sc.Web;
+    using Glass.Mapper.Sc.Web.Mvc;
+    using LionTrust.Feature.Carousel.Models;
+    using Sitecore.Mvc.Controllers;
+    using System.Web.Mvc;
+
+    public class FundScrollerController: SitecoreController
+    {
+        private readonly IMvcContext context;
+
+        public FundScrollerController(IMvcContext context)
+        {
+            this.context = context;
+        }
+
+        public ActionResult Render()
+        {
+            var datasource = context.GetDataSourceItem<IFundScroller>();
+            if (datasource == null)
+            {
+                return null;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/Feature/Carousel/website/Models/IFundScroller.cs
@@ -0,0 +1,12 @@
+﻿namespace LionTrust.Feature.Carousel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public interface IFundScroller: ICarouselGlassBase
+    {
+
+    }
+}
