a74b2aec1e218bb152581e573fbf7f74f43d6461
Max Rozvora <mr@exdst.com.ua>
2021-04-28 17:41:31 +0300

    fix carousel for mobile

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
@@ -8,9 +8,6 @@ SwiperCore.use([Navigation, Pagination]);
 
 export default () => {
   const swiper = new Swiper(".swiper-container-carousel", {
-    // effect: "coverflow",
-    // grabCursor: true,
-    slidesPerView: 3,
     watchSlidesVisibility: true,
     centeredSlides: true,
     breakpointsInverse: true,
@@ -18,50 +15,15 @@ export default () => {
     slideToClickedSlide: true,
     setWrapperSize: false,
     spaceBetween: 5,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 225,
-      depth: 200,
-      modifier: 1.8,
-      slideShadows: true,
-    },
+    loop: true,
     breakpoints: {
-      576: {},
-      768: {
-        coverflowEffect: {
-          rotate: 0,
-          stretch: 100,
-          depth: 600,
-          modifier: 1.1,
-          slideShadows: true,
-        },
+      0: {
+        slidesPerView: 1,
       },
       992: {
-        coverflowEffect: {
-          rotate: 0,
-          stretch: 225,
-          depth: 500,
-          modifier: 1.2,
-          slideShadows: true,
-        },
-      },
-      1200: {
-        coverflowEffect: {
-          modifier: 1.8,
-        },
-      },
-      1800: {
-        coverflowEffect: {
-          modifier: 1.8,
-        },
-      },
-      2560: {
-        stretch: 225,
-        depth: 300,
-        modifier: 1.5,
+        slidesPerView: 3,
       },
     },
-    loop: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
