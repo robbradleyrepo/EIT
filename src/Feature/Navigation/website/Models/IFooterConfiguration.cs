b808842b67258c19e18a9aaf5d69e0d4c8b3a434
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-28 13:53:12 +0300

    Refactorization for navigations to use FieldIDs at Glass mappings, removed CTA Labels and using CTALink text field

--- src/Feature/Navigation/website/Models/IFooterConfiguration.cs
+++ src/Feature/Navigation/website/Models/IFooterConfiguration.cs
@@ -10,49 +10,43 @@
         [SitecoreField(Constants.FooterConfiguration.FindUsLabel_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string FindUsLabel { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.Address_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.Address_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string Address { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.Location_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.Location_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string Location { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.PostalCode_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.PostalCode_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string PostalCode { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.GoogleMapsLink_FieldName, SitecoreFieldType.GeneralLink, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.GoogleMapsLink_FieldID, SitecoreFieldType.GeneralLink, "Footer")]
         Link GoogleMapsLink { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.GoogleMapsCTALabel_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
-        string GoogleMapsCTALabel { get; set; }
-
-        [SitecoreField(Constants.FooterConfiguration.GetInTouchLabel_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.GetInTouchLabel_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string GetInTouchLabel { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.PhoneNumber_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.PhoneNumber_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string PhoneNumber { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.Email_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.Email_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string Email { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.EmailCTALabel_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.EmailCTALabel_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string EmailCTALabel { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.SubscribeNewsletterLabel_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.SubscribeNewsletterLabel_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string SubscribeNewsletterLabel { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.SubscribeCTALabel_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
-        string SubscribeCTALabel { get; set; }
-
-        [SitecoreField(Constants.FooterConfiguration.SubscribeCTALink_FieldName, SitecoreFieldType.GeneralLink, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.SubscribeCTALink_FieldID, SitecoreFieldType.GeneralLink, "Footer")]
         Link SubscribeCTALink { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.Copyright_FieldName, SitecoreFieldType.SingleLineText, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.Copyright_FieldID, SitecoreFieldType.SingleLineText, "Footer")]
         string Copyright { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.PageLinks_FieldName, SitecoreFieldType.Treelist, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.PageLinks_FieldId, SitecoreFieldType.Treelist, "Footer")]
         IEnumerable<IPageLink> PageLink { get; set; }
 
-        [SitecoreField(Constants.FooterConfiguration.SocialLinks_FieldName, SitecoreFieldType.Treelist, "Footer")]
+        [SitecoreField(Constants.FooterConfiguration.SocialLinks_FieldId, SitecoreFieldType.Treelist, "Footer")]
         IEnumerable<ISocialIcon> SocialIcons { get; set; }
     }
 }
--- src/Feature/Navigation/website/Models/IHeaderDropdown.cs
+++ src/Feature/Navigation/website/Models/IHeaderDropdown.cs
@@ -7,22 +7,19 @@
 
     public interface IHeaderDropdown : INavigationGlassBase
     {
-        [SitecoreField(Constants.HeaderDropDown.PageItem_FieldName, SitecoreFieldType.DropTree, "Header Configuration")]
+        [SitecoreField(Constants.HeaderDropDown.PageItem_FieldID, SitecoreFieldType.DropTree, "Header Configuration")]
         INavigablePage PageItem { get; set; } 
 
-        [SitecoreField(Constants.HeaderDropDown.NavigationColumns_FieldName, SitecoreFieldType.Treelist, "Header Configuration")]
+        [SitecoreField(Constants.HeaderDropDown.NavigationColumns_FieldID, SitecoreFieldType.Treelist, "Header Configuration")]
         IEnumerable<IHeaderNavigationColumn> NavigationColumns { get; set; }
 
-        [SitecoreField(Constants.HeaderDropDown.Images_FieldName, SitecoreFieldType.Treelist, "Header Configuration")]
+        [SitecoreField(Constants.HeaderDropDown.Images_FieldID, SitecoreFieldType.Treelist, "Header Configuration")]
         IEnumerable<IHeaderImage> Images { get; set; }
 
-        [SitecoreField(Constants.HeaderDropDown.ShowCTA_FieldName, SitecoreFieldType.Checkbox, "Header Configuration")]
+        [SitecoreField(Constants.HeaderDropDown.ShowCTA_FieldID, SitecoreFieldType.Checkbox, "Header Configuration")]
         bool ShowCTA { get; set; }
 
-        [SitecoreField(Constants.HeaderDropDown.CTALabel_FieldName, SitecoreFieldType.SingleLineText, "Header Configuration")]
-        string CTALabel { get; set; }
-
-        [SitecoreField(Constants.HeaderDropDown.CTALink_FieldName, SitecoreFieldType.GeneralLink, "Header Configuration")]
+        [SitecoreField(Constants.HeaderDropDown.CTALink_FieldID, SitecoreFieldType.GeneralLink, "Header Configuration")]
         Link CTALink { get; set; }
     }
 }
