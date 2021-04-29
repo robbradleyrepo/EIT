932586625a0f646af680884e6c40af1c81028a32
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-29 10:13:03 +0300

    Merge conflicts fixes

---
+++ src/Feature/Navigation/website/Models/IQuickLinks.cs
@@ -0,0 +1,12 @@
+﻿namespace LionTrust.Feature.Navigation.Models
+{
+    using System.Collections.Generic;
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public interface IQuickLinks : INavigationGlassBase
+    {
+        [SitecoreField(Constants.QuickLinks.QuickLinksList_FieldName, SitecoreFieldType.Treelist, "Content")]
+        IEnumerable<IQuickLinkCTA> QuickLinksList { get; set; }
+    }
+}
