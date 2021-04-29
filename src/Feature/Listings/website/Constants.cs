66c82ddc91707d307d4a6a6b6fed013290288156
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-29 16:28:36 +0300

    HorizontalScroll implementation

--- src/Feature/Listings/website/Constants.cs
+++ src/Feature/Listings/website/Constants.cs
@@ -19,5 +19,32 @@
 
             public const string BackgroundColourFieldId = "{1841944B-D6A8-48CA-BAB1-071C8C7C603E}";
         }
+
+        public static class HorizontalScroll
+        {
+            public const string Title_FieldID = "{FD89A7AF-A29A-42DC-949C-5CE8F66F9C71}";
+            public const string Title_FieldName = "HorizontalScroll_Title";
+
+            public const string NumberOfItems_FieldID = "{08F9501F-35B1-4E66-96D0-8D04858A7EC5}";
+            public const string NumberOfItems_FieldName = "HorizontalScroll_NumberOfItems";
+        }
+
+        public static class ScrollerCard
+        {
+            public const string Title_FieldID = "{5B9F1D26-BDE7-4209-A71C-F0CEAC2600B5}";
+            public const string Title_FieldName = "ScrollCard_Title";
+
+            public const string Description_FieldID = "{4826908C-F8C8-43E4-8360-0C09ED0C732A}";
+            public const string Description_FieldName = "ScrollCard_Description";
+
+            public const string BackgroundImage_FieldID = "{05E19C75-F1A5-4591-BCF1-062C9EFFACC5}";
+            public const string BackgroundImage_FieldName = "ScrollCard_BackgroundImage";
+
+            public const string Link_FieldID = "{CE038003-3CA3-4B12-A32A-BB5E664C0595}";
+            public const string Link_FieldName = "ScrollCard_Link";
+
+            public const string LinkGoal_FieldID = "{BB76C1D8-044A-4813-B4DE-E36D21085304}";
+            public const string LinkGoal_FieldName = "ScrollCard_LinkGoal";
+        }
     }
 }
