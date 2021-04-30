33b8a494a488dd0beaa725da8e5392421f097b66
Max Rozvora <mr@exdst.com.ua>
2021-04-30 10:43:01 +0300

    finish page-anchor-link

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
@@ -5,8 +5,6 @@ export default () => {
   const sticky = navbar.offsetTop;
   const offset = navbar.offsetHeight;
 
-  console.log('offset', offset);
-
   function stickyNav() {
     if (window.pageYOffset >= sticky) {
       navbar.classList.add("sticky");
@@ -20,7 +18,7 @@ export default () => {
   let i = 0;
 
   Array.prototype.forEach.call(section, function (e) {
-    sections[e.id] = e.offsetTop - offset - 10
+    sections[e.id] = e.offsetTop - offset - 10;
   });
 
   window.onscroll = function () {
@@ -37,26 +35,47 @@ export default () => {
     }
   };
 
-  $("a.page-anchor__link").on('click', function(event) {
-
+  $("a.page-anchor__link").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
-
       // Store hash
       var hash = this.hash;
-
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
     } // End if
   });
 
+  const openBtn = $("#open-page-anchor");
+
+  openBtn.on("click", (e) => {
+    e.stopPropagation();
+    $(".page-anchor__links-mobile").stop().slideToggle();
+    openBtn.toggleClass("active");
+  });
+
+  document.addEventListener("click", () => {
+    $(".page-anchor__links-mobile").stop().slideUp();
+    openBtn.removeClass("active");
+  });
+
+  // $(window).resize(function () {
+  //   const win = $(this);
+  //   console.log("win", win);
+  //   if (win.width() >= 992) {
+  //     console.log("ture");
+  //     $(".page-anchor__links").stop().slideUp().css("display", "inline-block");
+  //   }
+  // });
 };
