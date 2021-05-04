7b65c4dbe5364ef73e57b3a295b12dd322f668a0
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-05-04 15:34:54 +0300

    LDC-798 Disclaimer Component implementation - reusing the same view layout as text, adjusted the text component template to be general and be reused by disclaimer

--- src/Feature/Text/website/Constants.cs
+++ src/Feature/Text/website/Constants.cs
@@ -2,15 +2,15 @@
 {
     public static class Constants
     {
-        public static class TextComponent
+        public static class RichText
         {
-            public const string Text_FieldName = "TextComponent_Text";
+            public const string Text_FieldName = "RichText_Text";
             public const string Text_FieldId = "{7A6EEA35-2025-43CF-A371-43D99A82E804}";
 
-            public const string SmallSize_FieldName = "TextComponent_SmallSize";
+            public const string SmallSize_FieldName = "RichText_SmallSize";
             public const string SmallSize_FieldId = "{B4D032F0-3342-49A1-AD8A-D0F640C59B4C}";
 
-            public const string TextColor_FieldName = "TextComponent_TextColor";
+            public const string TextColor_FieldName = "RichText_TextColor";
             public const string TextColor_FieldId = "{0422A25C-25C0-4B34-8CD5-8E39B76BBF3F}";
         }
 
