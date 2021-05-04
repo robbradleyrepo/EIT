158810b2257e5801c1ab873f6e640a46b8712943
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-05-04 11:56:30 +0100

    adding featured articles component

--- src/Feature/Article/website/Constants.cs
+++ src/Feature/Article/website/Constants.cs
@@ -2,6 +2,17 @@
 {
     public static class Constants
     {
+        public static class Link
+        {
+            public const string LinkFieldId = "{7DB38AE3-3B88-459D-8C62-DD656D7DEC8E}";
+        }
+
+        public static class FeaturedArticles
+        {
+            public const string HeadingFieldId = "{B2950181-27E6-4EEF-A1FA-329AA73FCE55}";
+            public const string ArticlesFieldId = "FeaturedArticles_Articles";
+        }
+
         public static class Article
         {
             public const string TopicsFieldId = "{4F9B280F-A22D-4E70-886A-45327E8AA507}";
---
+++ src/Feature/Article/website/Models/IFeaturedArticles.cs
@@ -0,0 +1,14 @@
+﻿namespace LionTrust.Feature.Article.Models
+{
+    using LionTrust.Foundation.ORM.Models;
+    using System.Collections.Generic;
+
+    public interface IFeaturedArticles: IGlassBase
+    {
+        string Heading { get; set; }
+
+        IEnumerable<IArticle> Articles { get; set; }
+
+        IEnumerable<ILink> Children { get; set; }
+    }
+}
