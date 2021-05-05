066e0175b5b488f0cc4b6fb3819cff5b334001ae
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-05-05 09:43:27 +0100

    adding featured articles

--- src/Feature/Article/website/Models/IFeaturedArticles.cs
+++ src/Feature/Article/website/Models/IFeaturedArticles.cs
@@ -1,12 +1,15 @@
 ﻿namespace LionTrust.Feature.Article.Models
 {
+    using Glass.Mapper.Sc.Configuration.Attributes;
     using LionTrust.Foundation.ORM.Models;
     using System.Collections.Generic;
 
     public interface IFeaturedArticles: IGlassBase
     {
+        [SitecoreField(Constants.FeaturedArticles.HeadingFieldId)]
         string Heading { get; set; }
 
+        [SitecoreField(Constants.FeaturedArticles.ArticlesFieldId)]
         IEnumerable<IArticle> Articles { get; set; }
 
         IEnumerable<ILink> Children { get; set; }
