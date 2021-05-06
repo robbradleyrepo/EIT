98f9cf888488f22ec4dde2c1e4c724b86323dd85
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-05-06 09:10:01 +0100

    add flag

--- tests/ui-tests/src/index.ts
+++ tests/ui-tests/src/index.ts
@@ -4,9 +4,9 @@ import * as chromeLauncher from 'chrome-launcher'
 
 (async () => {
   console.log('running');
-  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless', '--no-sandbox']});
   const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
-  const runnerResult = await lighthouse('https://www.liontrust.com', options);
+  const runnerResult = await lighthouse('https://www.liontrust.co.uk', options);
 
   // `.report` is the HTML report as a string
   const reportHtml = runnerResult.report;
