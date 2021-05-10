53a2e7b8a5019769d790886f1cabcfd214c19948
luke.wilkinson <luke.wilkinson@sagittarius-marketing.com>
2021-05-10 13:19:29 +0000

    Merged in feature/UI016-Fund-Manager-Component (pull request #60)
    create rendering for fund manager component
    
    * create rendering for fund manager component
    
    * fixing conflicts
    
    * fixing spaces in class name.
    
    * renaming interface with correct name.
    
    * removing constant not required.

---
+++ src/Feature/Fund/website/Models/IFundManagers.cs
@@ -0,0 +1,13 @@
+﻿namespace LionTrust.Feature.Fund.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using LionTrust.Foundation.Legacy.Models;
+    using LionTrust.Foundation.ORM.Models;
+    using System.Collections.Generic;
+
+    public interface IFundManagers : IGlassBase
+    {
+        [SitecoreField(Constants.FundManagers.FundManagersFieldId)]
+        IEnumerable<IAuthor> Managers { get; set; }
+    }
+}
