e4d6dc5751300795d454cd4e80fb386cf73997e4
Jason Cox <jason.cox@sagittarius-marketing.com>
2021-05-11 12:15:20 +0100

    adding component and moving anchor to foundation

--- src/Feature/Navigation/website/Constants.cs
+++ src/Feature/Navigation/website/Constants.cs
@@ -3,14 +3,7 @@
     using Sitecore.Data;
 
     public static class Constants
-    {
-        public static class Anchor
-        {
-            public const string AnchorFieldId = "{98B43E92-3166-4986-A181-E6EF91093454}";
-
-            public const string AnchorNameFieldId = "{AA0A9DE9-8D65-4233-A9D5-DFAB02BB9B5A}";
-        }
-
+    {     
         public static class CopyWithCta
         {
             public const string CopyFieldId = "{D7CD16BD-DBB5-44D9-AB26-8BBB1C9B7C2A}";
