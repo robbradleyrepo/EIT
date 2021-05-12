ae6a2d46bd6adef65b3cd3747e8d505f9a9e18ed
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-05-12 16:22:35 +0300

    Changed code part

--- src/Feature/Fund/website/Models/IFundExtended.cs
+++ src/Feature/Fund/website/Models/IFundExtended.cs
@@ -1,8 +1,28 @@
 ﻿namespace LionTrust.Feature.Fund.Models
 {
+    using System;
+
+    using Glass.Mapper.Sc.Configuration.Attributes;
     using LionTrust.Foundation.Legacy.Models;
 
-    public interface IFundExtended : IFund, IAdditionalInfoAndCharges, IKeyInfoPrice
+    public interface IFundExtended : IFund
     {
+        [SitecoreField(Constants.KeyInfoPrice.ClassLaunchDate_FieldId)]
+        DateTime ClassLaunchDate { get; set; }
+
+        [SitecoreField(Constants.KeyInfoPrice.Comparator_FieldId)]
+        string Comparator { get; set; }
+
+        [SitecoreField(Constants.KeyInfoPrice.Duration_FieldId)]
+        string Duration { get; set; }
+
+        [SitecoreField(Constants.KeyInfoPrice.OfferPrice_FieldId)]
+        string OfferPrice { get; set; }
+
+        [SitecoreField(Constants.KeyInfoPrice.SinglePrice_FieldId)]
+        string SinglePrice { get; set; }
+
+        [SitecoreField(Constants.KeyInfoPrice.PriceDate_FieldId)]
+        DateTime PriceDate { get; set; }
     }
 }
