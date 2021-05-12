b3c4bc4752adba1d0236414d3abbd9c2701d128d
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-05-12 11:07:01 +0300

    [LDC-797] Risk Banner initial implementation

---
+++ src/Feature/Fund/website/Models/IFundRisk.cs
@@ -0,0 +1,10 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace LionTrust.Feature.Fund.Models
+{
+    public interface IFundRisk : IFundGlassBase
+    {
+        [SitecoreField(Constants.FundRisk.BannerText_FieldId)]
+        string RiskBannerText { get; set; }
+    }
+}
