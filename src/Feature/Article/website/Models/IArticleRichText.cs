1657bb3d8a13597bab280946cc4c0da31550d3d5
jason.cox <jason.cox@sagittarius-marketing.com>
2021-05-17 16:05:22 +0000

    Merged in feature/article_text_components (pull request #89)
    adding text components
    
    * adding text components
    
    * moving using statements
    
    Approved-by: luke.wilkinson

---
+++ src/Feature/Article/website/Models/IArticleRichText.cs
@@ -0,0 +1,10 @@
+﻿namespace LionTrust.Feature.Article.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public interface IArticleRichText: IArticleGlassBase
+    {
+        [SitecoreField(Constants.ArticleRichText.CopyFieldId)]
+        string Copy { get; set; }
+    }
+}
