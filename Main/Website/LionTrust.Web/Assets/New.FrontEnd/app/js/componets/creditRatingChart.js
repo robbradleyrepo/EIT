ee4c4a784c2906dfa29e5e5f303aa0b0f8fd72c7
max <rozvora.max@gmail.com>
2021-05-19 11:38:52 +0300

    add some styles

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/creditRatingChart.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/creditRatingChart.js
@@ -69,6 +69,7 @@ export default () => {
           beginAtZero: true,
           ticks: {
             beginAtZero: true,
+            crossAlign: 'far'
           },
           grid: {
             borderDash: [3, 3],
