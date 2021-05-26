72956a670e980805e60db2a47bf66699dd8a50b3
Luke Wilkinson <luke.wilkinson@sagittarius.agency>
2021-05-26 16:57:01 +0100

    updating placeholder and body class.

--- src/Project/LionTrust/website/Constants.cs
+++ src/Project/LionTrust/website/Constants.cs
@@ -5,6 +5,8 @@
         public static class PageTypes
         {
             public const string HomeTemplateId = "{51D3A578-6E10-4EC6-BB5B-5C1307A6D661}";
+
+            public const string FundPageTemplateId = "{1F294BCB-1F17-4000-94BD-501A96BEC59A}";
         }
     }
 }
