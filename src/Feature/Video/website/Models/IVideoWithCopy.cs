c4c1b0cd8e30ede604e67f81762497e928f6b80e
jason.cox <jason.cox@sagittarius-marketing.com>
2021-06-03 11:43:32 +0000

    Merged in feature/adding_video_with_copy_component (pull request #184)
    adding video with copy component
    
    * adding video with copy component
    
    Approved-by: Bob Coppins

---
+++ src/Feature/Video/website/Models/IVideoWithCopy.cs
@@ -0,0 +1,17 @@
+﻿namespace LionTrust.Feature.Video.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using LionTrust.Foundation.Video;
+
+    public interface IVideoWithCopy: IVideoModel
+    {
+        [SitecoreField(Video.Constants.VideoWithCopy.HeadingFieldId)]
+        string Heading { get; set; }
+
+        [SitecoreField(Video.Constants.VideoWithCopy.CopyFieldId)]
+        string Copy { get; set; }
+
+        [SitecoreField(Video.Constants.VideoWithCopy.SubheadingFieldId)]
+        string Subheading { get; set; }
+    }
+}
