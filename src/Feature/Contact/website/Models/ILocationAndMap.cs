f6482efcabe4c93da9a391e7415a8c409d062281
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-06-07 19:25:08 +0300

    [LDC-807] Location and Map initial implementation

---
+++ src/Feature/Contact/website/Models/ILocationAndMap.cs
@@ -0,0 +1,19 @@
+﻿namespace LionTrust.Feature.Contact.Models
+{
+    using System.Collections.Generic;
+
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+    
+    public interface ILocationAndMap : IContactGlassBase
+    {
+        [SitecoreField(Constants.LocationAndMap.Heading_FieldId)]
+        string Heading { get; set; }        
+
+        [SitecoreField(Constants.LocationAndMap.SelectLabel_FieldId)]
+        string SelectLabel { get; set; }
+
+        [SitecoreChildren]
+        IEnumerable<IMapLink> MapLinks { get; set; }
+    }
+}
