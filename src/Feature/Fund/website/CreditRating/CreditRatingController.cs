e789861290e31577ff56f3b1d12acbfea6d9e93f
jason.cox <jason.cox@sagittarius-marketing.com>
2021-06-11 14:37:50 +0000

    Merged in feature/credit-rating (pull request #216)
    adding credit rating chart
    
    * adding credit rating chart
    
    * remove unused variable
    
    * null check the children
    
    Approved-by: luke.wilkinson

---
+++ src/Feature/Fund/website/CreditRating/CreditRatingController.cs
@@ -0,0 +1,35 @@
+﻿namespace LionTrust.Feature.Fund.CreditRating
+{
+    using Glass.Mapper.Sc.Web.Mvc;
+    using Newtonsoft.Json;
+    using Sitecore.Mvc.Controllers;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    public class CreditRatingController: SitecoreController
+    {
+        private readonly IMvcContext _context;
+
+        public CreditRatingController(IMvcContext context)
+        {
+            this._context = context;
+        }
+
+        public ActionResult Render()
+        {
+            var datasource = _context.GetDataSourceItem<ICreditRating>();
+            if (datasource == null)
+            {
+                return null;
+            }
+
+            if (datasource.Children != null)
+            {
+                var data = new { labels = datasource.Children.Select(c => c.RowName), data = datasource.Children.Select(c => c.Value), maxValue = datasource.MaxValue };
+                datasource.JsonDataObject = JsonConvert.SerializeObject(data);
+            }
+
+            return View("/views/fund/creditrating.cshtml", datasource);
+        }
+    }
+}
---
+++ src/Feature/Fund/website/CreditRating/ICreditRating.cs
@@ -0,0 +1,19 @@
+﻿namespace LionTrust.Feature.Fund.CreditRating
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using LionTrust.Feature.Fund.Models;
+    using System.Collections.Generic;
+
+    public interface ICreditRating: IFundGlassBase
+    {
+        [SitecoreField(Constants.CreditRating.HeadingFieldId)]
+        string Heading { get; set; }
+
+        IEnumerable<ICreditRatingRow> Children { get; set; }
+
+        [SitecoreField(Constants.CreditRating.MaxValueFieldId)]
+        string MaxValue { get;set; }
+
+        string JsonDataObject { get; set; }
+    }
+}
