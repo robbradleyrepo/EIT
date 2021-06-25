4b8347222e32f8b6798609f9aba35bd2d36239f7
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-06-25 15:31:28 +0300

    Adjusted FE css classes

--- src/Project/LionTrust/website/Constants.cs
+++ src/Project/LionTrust/website/Constants.cs
@@ -7,6 +7,8 @@
             public const string HomeTemplateId = "{51D3A578-6E10-4EC6-BB5B-5C1307A6D661}";
 
             public const string FundPageTemplateId = "{1F294BCB-1F17-4000-94BD-501A96BEC59A}";
+
+            public const string InsightPageTemplateId = "{2E71A4D9-8115-47E6-973A-7DE681370739}";
         }
     }
 }
