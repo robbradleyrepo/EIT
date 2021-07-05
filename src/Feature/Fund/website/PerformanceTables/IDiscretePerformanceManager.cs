249eca85857488dc2754032ffc62aab301fd2806
jason.cox <jason.cox@sagittarius-marketing.com>
2021-07-05 10:22:55 +0000

    Merged in feature/fund-components (pull request #325)
    adding discreteperformance component
    
    * adding discreteperformance component
    
    * refactor
    
    Approved-by: Richard Brisley

---
+++ src/Feature/Fund/website/PerformanceTables/IDiscretePerformanceManager.cs
@@ -0,0 +1,11 @@
+﻿namespace LionTrust.Feature.Fund.PerformanceTables
+{
+    using System.Collections.Generic;
+
+    public interface IDiscretePerformanceManager
+    {
+        IEnumerable<PerformanceTableRow> GetPerformanceTableRows(string citiCode);
+
+        PerformanceTableRow GetQuartile(string citiCode);
+    }
+}
---
+++ src/Feature/Fund/website/PerformanceTables/PerformanceTableRow.cs
@@ -0,0 +1,9 @@
+﻿namespace LionTrust.Feature.Fund.PerformanceTables
+{
+    public class PerformanceTableRow
+    {
+        public string Name { get; set; }
+
+        public double?[] Columns { get; set; }
+    }
+}
