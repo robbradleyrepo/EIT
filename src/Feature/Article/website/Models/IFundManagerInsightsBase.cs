77043d79bc9f01998e37982538d9c798d084bf66
luke.wilkinson <luke.wilkinson@sagittarius-marketing.com>
2021-07-14 11:36:21 +0000

    Merged in feature/BC025-Fund-Manager-Insights-Variation (pull request #361)
    adding fund manager insights for followed funds.
    
    * adding fund manager insights for followed funds.
    
    
    Approved-by: Gerhart Magdas

---
+++ src/Feature/Article/website/Models/IFundManagerInsightsBase.cs
@@ -0,0 +1,14 @@
+﻿namespace LionTrust.Feature.Article.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+    using LionTrust.Foundation.ORM.Models;
+    public interface IFundManagerInsightsBase : IGlassBase
+    {
+        [SitecoreField(Constants.FundManagerInsightsBase.CTA_FieldId)]
+        Link CTA { get; set; }
+
+        [SitecoreField(Constants.FundManagerInsightsBase.Heading_FieldId)]
+        string Heading { get; set; }
+    }
+}
