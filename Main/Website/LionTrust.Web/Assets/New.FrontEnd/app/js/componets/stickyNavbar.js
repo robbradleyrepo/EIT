8c84e6046fb20631a5eea18043406efc06f9f7b6
Max Rozvora <mr@exdst.com.ua>
2021-07-16 10:58:39 +0300

    fix scroll to position

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
@@ -44,14 +44,14 @@ export default () => {
       var hash = this.hash;
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
+      $("html, body").stop().animate(
         {
           scrollTop: $(hash).offset().top - offset,
         },
         800,
         function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+          history.pushState({}, '', hash);
         }
       );
     } // End if
