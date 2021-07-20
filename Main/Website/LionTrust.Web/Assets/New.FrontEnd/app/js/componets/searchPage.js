57200e67413672ef2695900078a6b583215076d8
Max Rozvora <mr@exdst.com.ua>
2021-07-20 16:09:29 +0300

    get data from api call

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/searchPage.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/searchPage.js
@@ -1,117 +1,22 @@
 import Vue from "vue/dist/vue.common.prod";
-import {pagination} from "./listFilter/mixins/pagination"
-const request = {
-  amountResults: "150",
-  results: [
-    {
-      link: "#link",
-      title: "Factsheet GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      date: "21st February 2021",
-      type: "Fund",
-      footer: {
-        factsheet: {
-          name: "Factsheet",
-          link: "#",
-        },
-        team: {
-          name: "The Global Fixed Income team",
-          link: "#",
-        },
-      },
-    },
-    {
-      link: "#link",
-      title: "Article GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      date: "21st February 2021",
-      type: "Article",
-      footer: {
-        author: {
-          name: "David Roberts",
-          imageSrc:
-            "images/components/search-page/David-Roberts-Liontrust@2x.png",
-        },
-        team: {
-          name: "The Global Fixed Income team",
-          link: "#",
-        },
-      },
-    },
-    {
-      link: "#link",
-      title: "Page GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Page",
-      date: "23st December 2020",
-    },
-    {
-      link: "#link",
-      title: "Document GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Document",
-      date: "18st June 2020",
-    },
-    {
-      link: "#link",
-      title: "Document GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Fund Manager",
-      date: "1st June 2019",
-    },
-    {
-      link: "#link",
-      title: "Article GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      date: "21st February 2021",
-      type: "Article",
-      footer: {
-        author: {
-          name: "David Roberts",
-          imageSrc:
-            "images/components/search-page/David-Roberts-Liontrust@2x.png",
-        },
-        team: {
-          name: "The Global Fixed Income team",
-          link: "#",
-        },
-      },
-    },
-    {
-      link: "#link",
-      title: "Page GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Page",
-      date: "23st December 2020",
-    },
-    {
-      link: "#link",
-      title: "Document GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Document",
-      date: "18st June 2020",
-    },
-    {
-      link: "#link",
-      title: "Document GF Absolute Return Bond Fund",
-      bodyText:
-        "Debt, debt, everywhere, nor any income to take. The current situation is not as bleak as this rewriting of Samuel Taylor Coleridge’s famous lines suggests, but with the UK and…",
-      type: "Fund Manager",
-      date: "1st June 2019",
-    },
-  ],
-};
+import { pagination } from "./listFilter/mixins/pagination";
+
+const location = "https://cm-liontrust-it.sagittarius.agency/";
+const rootDom = document.getElementById("result-list-app");
+let root = "";
+let host = rootDom.dataset.host;
+if (
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+) {
+  host = location + host;
+  root = location;
+} else {
+  host = "/" + host;
+}
+
 
 export default () => {
-  const host = "http://localhost:3004/results?";
   new Vue({
     el: "#result-list-app",
     mixins: [pagination],
@@ -122,7 +27,7 @@ export default () => {
       showPageInPagination: 7,
       loading: true,
       searchParams: {
-        queryText: "",
+        query: "",
         type: "",
         page: 1,
       },
@@ -143,6 +48,9 @@ export default () => {
       getSearchResult() {
         return this.results;
       },
+      getPage() {
+        return this.searchParams.page;
+      },
       getReqValue() {
         let match,
           pl = /\+/g, // Regex for replacing addition symbol with a space
@@ -162,19 +70,17 @@ export default () => {
         let query = "";
         const entries = Object.entries(this.searchParams);
         entries.forEach(([key, value], index) => {
-          if (value) query += `${key}=${value}`;
-          if (index !== entries.length - 1) query += "&";
+          if (value) query += `${key}=${value}&`;
         });
+        console.log('query',query);
         return query;
       },
-      
+
       getQueryText() {
-        return this.searchParams.queryText;
-      },      
-      
+        return this.searchParams.query;
+      },
     },
     methods: {
-      
       labelClick() {
         this.allResults = false;
       },
@@ -184,25 +90,21 @@ export default () => {
       },
       serchRequest(params = this.generateSearchParams) {
         this.changeUrl(params);
-        // uncomit it on develop
-        // $.ajax(host + params)
-        //   .done((res) => {
-        //     this.results = res;
-        //     console.log("results", this.results);
-        //     this.loading = false;
-        //   })
-        //   .fail((e) => {
-        //     this.loading = false;
-        //     throw new Error("Data does not fetch ", e);
-        //   });
+        const url = `${host}/search?query=article`
+        console.log('url',url);
+        $.ajax(url)
+          .done((request) => {
+            const { searchResults, amountResults } = request;
+            this.results = searchResults;
+            this.amountResults = amountResults;
+            console.log("request", request);
+            this.loading = false;
+          })
+          .fail((e) => {
+            this.loading = false;
+            throw new Error("Data does not fetch ", e);
+          });
 
-        // dummy data
-        setTimeout(() => {
-          const { results, amountResults } = request;
-          this.results = results;
-          this.amountResults = amountResults;
-          this.loading = false;
-        }, 500);
       },
       changeUrl(searchParams) {
         window.history.pushState(
@@ -210,7 +112,7 @@ export default () => {
           "search",
           `${window.location.href.split("?")[0]}?${searchParams}`
         );
-      },      
+      },
     },
     watch: {
       allResults() {
@@ -236,7 +138,7 @@ export default () => {
       },
     },
     mounted() {
-      this.searchParams.queryText = this.getReqValue.queryText;
+      this.searchParams.query = this.getReqValue.query;
       this.serchRequest();
     },
   });
