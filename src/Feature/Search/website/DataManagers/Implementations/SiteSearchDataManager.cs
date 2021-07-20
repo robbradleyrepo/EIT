dfd515f3c8d755ba95fb906553107063fa87e363
Luke Wilkinson <luke.wilkinson@sagittarius.agency>
2021-07-20 11:19:37 +0100

    adding try / catch and logging.

--- src/Feature/Search/website/DataManagers/Implementations/SiteSearchDataManager.cs
+++ src/Feature/Search/website/DataManagers/Implementations/SiteSearchDataManager.cs
@@ -16,6 +16,7 @@
     using LionTrust.Foundation.Search.Models.ContentSearch;
     using SiteSearchResultItem = SiteSearch.SiteSearchResultItem;
     using System;
+    using Sitecore.Diagnostics;
 
     public class SiteSearchDataManager : ISiteSearchDataManager
     {
@@ -32,57 +33,68 @@
 
         public ISearchResponse<SiteSearchHit> Search(string query, string database, string[] templatesIds, string language, int resultsPerPage, int page)
         {
-            var startPage = resultsPerPage * (page - 1);
-            using (IProviderSearchContext context = ContentSearchManager
-                                                            .GetIndex($"liontrust_search_{database}_index")
-                                                            .CreateSearchContext(SearchSecurityOptions.DisableSecurityCheck))
+            var SearchResponse = new SearchResponse<SiteSearchHit>();
+            try
             {
-
-                var predicate = new LocalDatasourceQueryPredicateProvider<SiteSearchResultItem>().GetQueryPredicate(new BasicQuery { QueryText = query });
-                var legacyPredicate = new LegacyQueryPredicateProvider<SiteSearchResultItem>().GetQueryPredicate(new BasicQuery { QueryText = query });
-
-                predicate = predicate.Or(legacyPredicate);
-                if (templatesIds != null && templatesIds.Length > 0)
+                var startPage = resultsPerPage * (page - 1);
+                using (IProviderSearchContext context = ContentSearchManager
+                                                                .GetIndex($"liontrust_search_{database}_index")
+                                                                .CreateSearchContext(SearchSecurityOptions.DisableSecurityCheck))
                 {
-                    var templateQuery = PredicateBuilder.True<SiteSearchResultItem>();
-                    foreach (var item in templatesIds)
+
+                    var predicate = new LocalDatasourceQueryPredicateProvider<SiteSearchResultItem>().GetQueryPredicate(new BasicQuery { QueryText = query });
+                    var legacyPredicate = new LegacyQueryPredicateProvider<SiteSearchResultItem>().GetQueryPredicate(new BasicQuery { QueryText = query });
+
+                    predicate = predicate.Or(legacyPredicate);
+                    if (templatesIds != null && templatesIds.Length > 0)
                     {
-                        templateQuery = templateQuery.Or(s => s.TemplateId == new ID(item));
+                        var templateQuery = PredicateBuilder.True<SiteSearchResultItem>();
+                        foreach (var item in templatesIds)
+                        {
+                            templateQuery = templateQuery.Or(s => s.TemplateId == new ID(item));
+                        }
+
+                        predicate = predicate.And(templateQuery);
+                    }
+                    var searchQuery = context.GetQueryable<SiteSearchResultItem>()
+                        .Where(predicate)
+                        .Where(r => r.Language == language)
+                        .Skip(startPage)
+                        .Take(resultsPerPage);
+
+                    var results = searchQuery.GetResults();
+
+                    if (results == null)
+                    {
+                        return null;
                     }
 
-                    predicate = predicate.And(templateQuery);
+                    var contentSearchResults = new ContentSearchResults<SiteSearchResultItem> { SearchResults = results, TotalResults = results.TotalSearchResults };
+
+
+                    if (contentSearchResults.TotalResults > 0)
+                    {
+                        SearchResponse.SearchResults = MapFundResultHits(contentSearchResults.SearchResults);
+                        SearchResponse.StatusMessage = "Success";
+                        SearchResponse.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                        SearchResponse.TotalResults = contentSearchResults.TotalResults;
+                    }
+                    else
+                    {
+                        SearchResponse.StatusMessage = "No search results found";
+                        SearchResponse.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
+                    }
                 }
-                var searchQuery = context.GetQueryable<SiteSearchResultItem>()
-                    .Where(predicate)
-                    .Where(r => r.Language == language)
-                    .Skip(startPage)
-                    .Take(resultsPerPage);
-
-                var results = searchQuery.GetResults();
-
-                if (results == null)
-                {
-                    return null;
-                }
-
-                var contentSearchResults = new ContentSearchResults<SiteSearchResultItem> { SearchResults = results, TotalResults = results.TotalSearchResults };
-
-                var SearchResponse = new SearchResponse<SiteSearchHit>();
-                if (contentSearchResults.TotalResults > 0)
-                {
-                    SearchResponse.SearchResults = MapFundResultHits(contentSearchResults.SearchResults);
-                    SearchResponse.StatusMessage = "Success";
-                    SearchResponse.StatusCode = 200;
-                    SearchResponse.TotalResults = contentSearchResults.TotalResults;
-                }
-                else
-                {
-                    SearchResponse.StatusMessage = "No search results found";
-                    SearchResponse.StatusCode = 404;
-                }
-
-                return SearchResponse;
             }
+            catch (Exception ex)
+            {
+                SearchResponse.StatusMessage = "An error occured when trying search";
+                SearchResponse.StatusCode = (int)System.Net.HttpStatusCode.InternalServerError;
+                Log.Error(SearchResponse.StatusMessage, ex, this);
+            }
+
+            return SearchResponse;
+
         }
 
         private IEnumerable<SiteSearchHit> MapFundResultHits(IEnumerable<SearchHit<SiteSearchResultItem>> hits)
