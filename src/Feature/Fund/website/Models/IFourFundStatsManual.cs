a3b8e7e8abe0d412840ed8c0c203524d09025b1b
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-07-22 18:47:35 +0300

    Initial implementation for four fund stats

---
+++ src/Feature/Fund/website/Models/IFourFundStatsManual.cs
@@ -0,0 +1,34 @@
+﻿namespace LionTrust.Feature.Fund.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public interface IFourFundStatsManual : IFundGlassBase
+    {
+        [SitecoreField(Constants.FourFundStatsManual.Heading_FieldId)]
+        string Heading { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.FirstTitle_FieldId)]
+        string FirstTitle { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.FirstValue_FieldId)]
+        string FirstValue { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.SecondTitle_FieldId)]
+        string SecondTitle { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.SecondValue_FieldId)]
+        string SecondValue { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.ThirdTitle_FieldId)]
+        string ThirdTitle { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.ThirdValue_FieldId)]
+        string ThirdValue { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.FourthTitle_FieldId)]
+        string FourthTitle { get; set; }
+
+        [SitecoreField(Constants.FourFundStatsManual.FourthValue_FieldId)]
+        string FourthValue { get; set; }
+    }
+}
