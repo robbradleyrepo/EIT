6dc161b7f91a33bbf18fc10c7b16aad2abb64aea
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-07-26 21:36:23 +0300

    BC-044 Table implementation

---
+++ src/Feature/Text/website/Models/ITable.cs
@@ -0,0 +1,13 @@
+﻿namespace LionTrust.Feature.Text.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+
+    public interface ITable : ITextGlassBase
+    {
+        [SitecoreField(Constants.Table.Text_FieldId)]
+        string Text { get; set; }
+
+        [SitecoreField(Constants.Table.Value_FieldId)]
+        string Value { get; set; }
+    }
+}
