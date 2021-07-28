55d2bbfa75525ef1f8597516b0dc5f2cc95121de
Max Rozvora <mr@exdst.com.ua>
2021-07-28 15:41:26 +0300

    finish BC042-Document-Lister-(Variation).html

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/tabNav.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/tabNav.js
@@ -1,10 +1,32 @@
-export default () => {
-    $('[data-tab-target]').on('click', function() {
-        const el = $(this);
-        const id = el.attr('data-tab-target');
-        $('[data-tab-target]').removeClass('active');
-        el.addClass('active');
-        $('[role="tabelement"]').removeClass('active');
-        $(`#${id}`).addClass('active');
-    })
-}
+export default () => {    
+
+  // click on tab
+  $("[data-tab-target]").on("click", function () {
+    const el = $(this);
+    const id = el.attr("data-tab-target");
+    const parentEl = el.parents('[data-select-for]').attr('data-select-for');
+    $(`#${parentEl} [data-tab-target]`).removeClass("active");
+    el.addClass("active");
+    changeTab(id, parentEl); 
+    $(`#${parentEl} .document-select-filter`).val(id);
+  });
+
+  // change select
+  $(".document-select-filter").on("change", function (e) {
+    const parent = $(this).attr("data-select-for");
+    const id = e.target.value;
+    changeTab(id);
+    $(`#${parent} [data-tab-target]`).removeClass("active");
+    $(`#${parent} [data-tab-target="${id}"]`).addClass("active");
+  });
+
+  function changeTab(id, parent) {
+    const tab = $(`#${id}`);
+    if (tab.length) {
+      $(`#${parent} [role="tabelement"]`).removeClass("active");
+      tab.addClass("active");
+    } else {
+      console.error("Tab not found");
+    }
+  }
+};
