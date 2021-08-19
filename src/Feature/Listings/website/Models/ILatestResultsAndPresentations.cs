74bd43ccdf37c10572cc2d3ec5773726622d65a5
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-08-19 12:14:12 +0300

    Initial implementation

---
+++ src/Feature/Listings/website/Models/ILatestResultsAndPresentations.cs
@@ -0,0 +1,38 @@
+﻿namespace LionTrust.Feature.Listings.Models
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Glass.Mapper.Sc.Fields;
+    
+    public interface ILatestResultsAndPresentations : IListingsGlassBase
+    {
+        [SitecoreField(Constants.LatestResultsAndPresentation.Title_FieldId)]
+        string Title { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.TitleColor_FieldId)]
+        Foundation.Design.ILookupValue TitleColor { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.ViewAllLabel_FieldId)]
+        string ViewAllLabel { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.AllResultsPage_FieldId)]
+        IListingPageWithChildren AllResultsPage { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.FirstImage_FieldId)]
+        Image FirstImage { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.SecondImage_FieldId)]
+        Image SecondImage { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.ThirdImage_FieldId)]
+        Image ThirdImage { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.PressReleaseIcon_FieldId)]
+        Image PressReleaseIcon { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.ReportIcon_FieldId)]
+        Image ReportIcon { get; set; }
+
+        [SitecoreField(Constants.LatestResultsAndPresentation.PresentationIcon_FieldId)]
+        Image PresentationIcon { get; set; }
+    }
+}
