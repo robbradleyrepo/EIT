8c499b3eb1673102d18b1332611439e25de12dbb
Max Rozvora <mr@exdst.com.ua>
2021-08-27 11:08:36 +0000

    Merged in feature/fix_investment_slider_markup (pull request #589)
    fix investment card markup
    
    * fix investment card markup
    
    
    Approved-by: luke.wilkinson

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/investmentCard.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/investmentCard.js
@@ -1,42 +1,8 @@
 import Swiper from 'swiper';
 
 export default () => {    
-    const swiper = new Swiper('#investment-slider', {
-        slidesPerView: 1.2,
-        spaceBetween: 32,
-        slidesOffsetAfter: 320,
-        breakpointsInverse: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-                spaceBetween: 8,
-            },
-            576: {
-                slidesPerView: 1.5,
-                spaceBetween: 8,
-            },
-            768: {
-                slidesPerView: 2.2,
-                spaceBetween: 8,
-            },
-            992: {
-                slidesPerView: 2.8,
-                spaceBetween: 10,
-            },
-            1200: {
-                slidesPerView: 3.5,
-                spaceBetween: 32
-            },
-            1400: {
-                slidesPerView: 4,
-                spaceBetween: 32,
-                slidesOffsetAfter: 700,
-            },
-            2560: {
-                slidesPerView: 4.5,
-                spaceBetween: 32,
-                slidesOffsetAfter: 700,
-            }
-        }
+    new Swiper('#investment-slider', {
+        slidesPerView: 'auto',
+        breakpointsInverse: true, 
       });
 }
