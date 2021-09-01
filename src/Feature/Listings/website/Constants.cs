6ba5e7a669bdd1be3532335e40a3520506d7aba8
radwan ayoub <radwan.ayoub@sagittarius-marketing.com>
2021-09-01 08:56:55 +0000

    Merged in feature/LDC-1520-TextShouldBeBlackColor (pull request #583)
    LDC-1520-updated accordion title HTML markup for black text
    
    * updated accordion HTML markup for black text
    
    * updated Accordion heading color
    
    
    Approved-by: luke.wilkinson
    Approved-by: Bob Coppins

--- src/Feature/Listings/website/Constants.cs
+++ src/Feature/Listings/website/Constants.cs
@@ -5,10 +5,9 @@
         public static class Accordion
         {
             public const string HeadingField = "{38E1ED19-3192-47C2-A92B-55A8D15280D0}";
-
             public const string ImageField = "{8FC7A09F-043C-41B7-BCF5-C8633F058F99}";
-
             public const string CopyField = "{E21C6470-0045-4FB7-8CE1-3B72F54D7CA4}";
+            public const string TextColorField = "{3493D959-3FF5-471A-A29C-06020BB7E641}";
         }
 
         public static class AccordionRenderingParameters
