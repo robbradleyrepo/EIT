36d0c7c94bbdce26344f99c05593b410d71d51be
luke.wilkinson <luke.wilkinson@sagittarius-marketing.com>
2021-09-13 15:47:08 +0000

    Merged in bugfix/primary-nav-gated (pull request #631)
    adding missing code from gated access for the primary nav.
    
    * adding missing code from gated access for the primary nav.
    
    * changed to use extension method.
    
    
    Approved-by: Bob Coppins

---
+++ src/Foundation/Navigation/website/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace LionTrust.Foundation.Navigation
+{
+    using LionTrust.Foundation.Navigation.Models;
+    using LionTrust.Foundation.Onboarding.Helpers;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Extensions
+    {
+        public static IEnumerable<INavigablePage> WithAccessTo(this IEnumerable<INavigablePage> pages)
+        {
+            return pages.Where(x => x.Fund == null || OnboardingHelper.HasAccess(x.Fund.ExcludedCountries));
+        }
+    }
+}
