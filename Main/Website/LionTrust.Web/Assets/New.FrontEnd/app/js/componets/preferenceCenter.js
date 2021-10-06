405ece4a5f8f5cace2cd4e965b420383f6dca215
Tasneem Khlaifat <tasneem.khlaifat@sagittarius-marketing.com>
2021-10-06 14:50:14 +0000

    Merged in LB-440 (pull request #665)
    LB-440 fix UI issues
    
    * LB-440 fix UI issues
    
    
    Approved-by: luke.wilkinson

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/preferenceCenter.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/preferenceCenter.js
@@ -1,23 +1,42 @@
- export default () => {
-     var $list = $('.multi-select-card__group ');
-     $list.each(function() {
-         var $listItems = $(this).find(".multi-select-card__row");
-         var numInList = $listItems.length;
-         var checkedList = $list.find('input:checked').length;
-         console.log(checkedList);
-         $('.slected_count').text(checkedList);
-         console.log(numInList);
-         $($listItems).parents('.accordion__card').find('.total_count').text(numInList);
-         $listItems.find("input").on('change', function() {
-             if ($(this).parents('.accordion__card').find('.multi-select-card__group input:checked').length == $(this).parents('.accordion__card').find('.multi-select-card__group input').length) {
-                 $(this).parents('.accordion__card').find('.multi-select-card__allSelected input').attr('checked', 'checked');
-                 //alert('all selected');
-             } else {
-                 $(this).parents('.accordion__card').find('.multi-select-card__allSelected input').removeAttr('checked');
-             }
-             checkedList = $listItems.find('input:checked').length;
-             console.log(checkedList);
-             $listItems.parents('.accordion__card').find('.slected_count').text(checkedList);
-         });
-     });
- }
+export default () => {
+    var $list = $('.multi-select-card__group ');
+    var yesIsChecked = $('.can-toggle input');
+    var selectAllList = $('.multi-select-card__allSelected input');
+    $list.each(function() {
+        var $listItems = $(this).find(".multi-select-card__row");
+        var numInList = $listItems.length;
+        var checkedList = $list.find('input:checked').length;
+        console.log(checkedList);
+        $('.slected_count').text(checkedList);
+        console.log(numInList);
+        $($listItems).parents('.accordion__card').find('.total_count').text(numInList);
+        $listItems.find("input").on('change', function() {
+            if ($(this).parents('.accordion__card').find('.multi-select-card__group input:checked').length == $(this).parents('.accordion__card').find('.multi-select-card__group input').length) {
+                $(this).parents('.accordion__card').find('.multi-select-card__allSelected input').prop('checked', this.checked);
+                //alert('all selected');
+            } else {
+                $(this).parents('.accordion__card').find('.multi-select-card__allSelected input').prop('checked', false);
+            }
+            checkedList = $listItems.find('input:checked').length;
+            console.log(checkedList);
+            $listItems.parents('.accordion__card').find('.slected_count').text(checkedList);
+        });
+    });
+    $(selectAllList).on('change', function() {
+        $(this).parents('.multi-select-card').find('.multi-select-card__group input').prop('checked', this.checked).trigger('change');
+    })
+    $('.showPreferencesForms').on('click', function(e) {
+        e.preventDefault();
+        $('.retrive-preferences-form').toggleClass('retrive-preferences-form__show');
+    })
+    $('.yes-is-checked').addClass('yes-is-checked__show');
+    yesIsChecked.on('change', function() {
+        if (yesIsChecked.is(':checked')) {
+            $('.yes-is-checked').addClass('yes-is-checked__show');
+            $('.no-is-checked').removeClass('no-is-checked__show');
+        } else {
+            $('.yes-is-checked').removeClass('yes-is-checked__show');
+            $('.no-is-checked').addClass('no-is-checked__show');
+        }
+    });
+}
