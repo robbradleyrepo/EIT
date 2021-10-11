75e756f2e461e79ee170882cd4ac0cd01a083799
Tasneem Khlaifat <tasneem.khlaifat@sagittarius-marketing.com>
2021-10-11 08:54:06 +0000

    Merged in LDC-734-fix (pull request #669)
    if I click on the arrow in the CTA it leads nowhere  --- Fix with style
    
    * if I click on the arrow in the CTA it leads nowhere  --- Fix with style
    
    * Merge branch 'develop' of https://bitbucket.org/sagittarius-marketing/liontrustfrontend into LDC-734-fix
    
    # Conflicts:
    #	Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/styles/sass/blocks/_onboarding-overlay.sass
    
    
    Approved-by: luke.wilkinson

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/onboarding.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/onboarding.js
@@ -1,62 +1,48 @@
 // Onboarding overlay modal
 import Cookies from "js-cookie";
-
 export default () => {
     const onboarding = $(".onboarding-overlay");
     const btnStep = $("[data-set-step]");
     const rawAcceptText = $('.onboarding-overlay__text').text();
     var countryName = '';
-    var onBoardingBtn = $('.onboarding-overlay__set-step');
-
     // default values
     const currentTab = Cookies.get("currentTab") || 0;
-
     const showTab = (currentTab) => {
         const tabs = $("[data-tab-number]");
         tabs.removeClass("visible");
         $(tabs[currentTab]).addClass("visible");
-
         const step = $(".onboarding-overlay__step");
         step.removeClass("active");
-
         if (currentTab == 0 || currentTab == 1) $(step[0]).addClass("active");
         if (currentTab == 2) $(step[1]).addClass("active");
         if (currentTab == 3) $(step[2]).addClass("active");
-
         Cookies.set("currentTab", currentTab);
     };
-
     if (onboarding.hasClass('active')) {
         $('body').addClass('overflow-hidden');
         showTab(currentTab);
     }
-
     // move to next step
     btnStep.on("click", function(e) {
         e.preventDefault();
         if (e.target.dataset.isoCountry) {
             SetCountry(e);
         }
-
         const tab = $(this).data('setStep');
         showTab(tab);
     });
-
     // navigation on tabs
     $('[data-change-step]').on("click", (e) => {
         changeStep(e);
     });
-
     // set country
     $(".set-location__item").on("click", (e) => {
         SetCountry(e);
         showTab(2);
     });
-
     $('.onboarding-overlay__link').on('click', function() {
         $('.onboarding-overlay__scroller').slideToggle();
     })
-
     const changeStep = (e) => {
         e.preventDefault();
         var tab = e.target.dataset.changeStep;
@@ -66,16 +52,13 @@ export default () => {
         }
         showTab(tab);
     }
-
     const SetCountry = (e) => {
         $('#Country').val(e.target.dataset.isoCountry);
         countryName = e.target.dataset.nameCountry;
-
         $.ajax({
             url: "/api/sitecore/Onboarding/GetInvestorRoles?countryIso=" + e.target.dataset.isoCountry
         }).done(function(data) {
             $(".choose-investor-role").html(data);
-
             if (e.target.dataset.isoCountry != "GB") {
                 $('.onboarding-overlay__title.uk-title').hide();
                 $('.onboarding-overlay__title.non-uk-title').show();
@@ -83,19 +66,15 @@ export default () => {
                 $('.onboarding-overlay__title.uk-title').show();
                 $('.onboarding-overlay__title.non-uk-title').hide();
             }
-
             $("[data-investor-type]").on("click", (e) => {
                 e.preventDefault();
-
                 $('#InvestorId').val(e.target.dataset.investorType);
                 var acceptText = $('.onboarding-overlay__text');
                 $(acceptText).text(rawAcceptText.replace("{role}", e.target.dataset.investorName).replace("{country}", countryName));
-
                 const tab = e.target.dataset.setStep;
                 showTab(tab);
             });
         });
-
         $.ajax({
             url: "/api/sitecore/Onboarding/GetTermsAndConditions?countryIso=" + e.target.dataset.isoCountry
         }).done(function(data) {
@@ -103,11 +82,4 @@ export default () => {
             $('.onboarding-overlay__scroller').show();
         });
     }
-
-    onBoardingBtn.each(function() {
-        var onBoardingNextStep = $(this).attr('data-set-step');
-        $(this).find('.icon-arrow-right').attr('data-set-step', onBoardingNextStep);
-        //console.log(onBoardingNextStep);
-
-    })
 };
