4ba52838d6d8e18f2b1fa3591fa85b4902968787
Tasneem Khlaifat <tasneem.khlaifat@sagittarius-marketing.com>
2021-10-13 08:39:38 +0000

    Merged in LDC-1490 (pull request #676)
    LDC-1490 Alignments wrong on fund detail page
    
    * LDC-1490 Alignments wrong on fund detail page
    
    * LDC-1519 (CSD) Heading text should be gold and body text should be black colour and section starts from middle of the page
    
    
    Approved-by: luke.wilkinson

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/listing-page.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/listing-page.js
@@ -1,7 +1,13 @@
 import listFilter from "./componets/listFilter";
-
-document.addEventListener("DOMContentLoaded", () => {  
-if($('#lister-app').length > 0)
-    listFilter();
+document.addEventListener("DOMContentLoaded", () => {
+    if ($('#lister-app').length > 0)
+        listFilter();
+    var anchorLinks = $('.page-anchor-link .page-anchor .page-anchor__right:not(.page-anchor__right-mobile) .page-anchor__links .page-anchor__link');
+    var anchorLength = anchorLinks.length;
+    console.log(anchorLength);
+    if (anchorLength > 4) {
+        $(anchorLinks).parent('.page-anchor__links').addClass('d-flex justify-content-between');
+    } else {
+        $(anchorLinks).parent('.page-anchor__links').addClass('d-flex justify-content-start m-link');
+    }
 });
-
