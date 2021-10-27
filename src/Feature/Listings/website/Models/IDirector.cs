e2286d2b016889bbc5064f0db459d0e353030fd9
Cristina <cristina.dragos@deleteagency.com>
2021-10-27 21:40:40 +0300

    add the direct line field on the director item and display it on the directors list component

--- src/Feature/Listings/website/Models/IDirector.cs
+++ src/Feature/Listings/website/Models/IDirector.cs
@@ -20,11 +20,17 @@
         [SitecoreField(Constants.Director.Email_FieldId)]
         string Email { get; set; }
 
+        [SitecoreField(Constants.Director.DirectLine_FieldId)]
+        string DirectLine { get; set; }
+
         [SitecoreField(Constants.Director.MobileNumber_FieldId)]
         string MobileNumber { get; set; }
 
         [SitecoreField(Constants.Director.EmailLabel_FieldId)]
         string EmailLabel { get; set; }
+
+        [SitecoreField(Constants.Director.DirectLineLabel_FieldId)]
+        string DirectLineLabel { get; set; }
 
         [SitecoreField(Constants.Director.MobileLabel_FieldId)]
         string MobileLabel { get; set; }
