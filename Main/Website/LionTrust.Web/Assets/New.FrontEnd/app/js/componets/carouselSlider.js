df683bc57b62cce998c574b61c5fd214c6bc58fc
Tasneem Khlaifat <tasneem.khlaifat@velir.com>
2021-11-14 10:31:49 +0200

    LDC-1272 | LDC-1523 | LDC-1678

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/carouselSlider.js
@@ -22,5 +22,15 @@ export default () => {
       el: ".swiper-pagination",
       clickable: true,
     },
+    on: {
+      slideChange: function () {
+        var activeIndex = this.activeIndex;
+        var realIndex = this.slides.eq(activeIndex).attr('data-swiper-slide-index');
+       $('.swiper-slide').removeClass('previousActiveClass nextActiveClass swiper-slide-duplicate-prev swiper-slide-prev swiper-slide-duplicate-next swiper-slide-next');
+       $('.swiper-slide[data-swiper-slide-index="'+realIndex+'"]').removeClass('previousActiveClass nextActiveClass swiper-slide-prev swiper-slide-duplicate-next');
+       $('.swiper-slide[data-swiper-slide-index="'+realIndex+'"]').prev().addClass('previousActiveClass');
+       $('.swiper-slide[data-swiper-slide-index="'+realIndex+'"]').next().addClass('nextActiveClass');
+      },
+    }
   });
 };
