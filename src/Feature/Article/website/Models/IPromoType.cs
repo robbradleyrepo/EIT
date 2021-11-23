45f59541584e5b4bda22e37d0f0799748247f1f5
Cristina <cristina.dragos@deleteagency.com>
2021-11-23 11:39:31 +0200

    add an article type field on the promo type template in order to have a matche between promo content type and blog type; test items

---
+++ src/Feature/Article/website/Models/IPromoType.cs
@@ -0,0 +1,13 @@
+﻿namespace LionTrust.Feature.Article.Models
+{
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using LionTrust.Foundation.ORM.Models;
+    using System;
+
+    public interface IPromoType : IGlassBase
+    {
+        [SitecoreField(Constants.PromoType.ArticleType_FieldId, SitecoreFieldType.Droplink)]
+        Guid ArticleType { get; set; }    
+    }
+}
