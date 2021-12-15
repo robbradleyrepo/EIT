7a3f8f97d0285722239bcbed118b6562e99c135a
Tasneem Khlaifat <tasneem.khlaifat@velir.com>
2021-12-15 13:39:29 +0200

    fix sticky bar

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/listing-page.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/listing-page.js
@@ -2,13 +2,13 @@ import listFilter from "./componets/listFilter";
 document.addEventListener("DOMContentLoaded", () => {
     if ($('#lister-app').length > 0)
         listFilter();
-    var anchorLinks = $('.page-anchor-link .page-anchor .page-anchor__right:not(.page-anchor__right-mobile) .page-anchor__links .page-anchor__link');
+    var anchorLinks = $('.scroll-nav__list  .scroll-nav__item');
     var anchorLength = anchorLinks.length;
     console.log(anchorLength);
     if (anchorLength > 4) {
-        $(anchorLinks).parent('.page-anchor__links').addClass('d-flex justify-content-between');
+        $(anchorLinks).parent('.scroll-nav__list').addClass('d-flex justify-content-between');
     } else {
-        $(anchorLinks).parent('.page-anchor__links').addClass('d-flex justify-content-start m-link');
+        $(anchorLinks).parent('.scroll-nav__list').addClass('d-flex justify-content-start m-link');
     }
     window.onload = function () {
         var imageSrcBg = document.getElementsByClassName('video-box__link');
