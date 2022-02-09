3d5228bc619ffa6f81db1a65a58fbc26b1c7d329
Tasneem Khlaifat <tasneem.khlaifat@sagittarius.agency>
2022-02-09 16:16:18 +0200

    Remove Commented lines

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/componets/stickyNavbar.js
@@ -4,8 +4,6 @@ export default () => {
   const sticky = navbar.offset()?.top;
   var sectionToScroll = $(document).find('.section-spy');
   var sectionHash = $(document).find('.page-anchor__right .page-anchor__link');
-
-
   sectionToScroll.each(function (index) {
     var addID = $(sectionToScroll[index]).attr('id');
     $(sectionToScroll[index]).nextUntil('.section-spy').wrapAll('<div class="hash-section"/>')
@@ -13,11 +11,9 @@ export default () => {
     var nextOfHash = $(sectionToScroll[index]).next()
     $(nextOfHash).attr('id', addID);
   });
-  if($('.page-anchor-link').length)
-  {
+  if ($('.page-anchor-link').length) {
     var stickyPostion = $('.page-anchor-link').offset().top;
   }
-  
   function stickyNav() {
     if (window.pageYOffset >= stickyPostion) {
       $(".page-anchor-link").addClass("sticky");
@@ -37,19 +33,13 @@ export default () => {
     openBtn.removeClass("active");
   });
   (function ($) {
-
-    var hashTagOffest,hashTag;
-    $('.page-anchor__links a').on('click', function(){
-       hashTag= $(this).attr('href');
-       hashTagOffest= $(hashTag).offset().top;
-       console.log(hashTagOffest);
-      
+    var hashTagOffest, hashTag;
+    $('.page-anchor__links a').on('click', function () {
+      hashTag = $(this).attr('href');
+      hashTagOffest = $(hashTag).offset().top;
+      console.log(hashTagOffest);
     })
     $(window).on("load", function () {
-      // $(".hero-manager-link__link:nth-child(1), .hero-manager-link__link:nth-child(2), .hero-manager-link__link:nth-child(3), .hero-manager-link__link:nth-child(4)").wrapAll('<div/>');
-      // $(".hero-manager-link__link:nth-child(5), .hero-manager-link__link:nth-child(6), .hero-manager-link__link:nth-child(7)").wrapAll('<div/>');
-      // $(".hero-manager-link__link:nth-child(8), .hero-manager-link__link:nth-child(9)").wrapAll('<div/>');
-
       $("#sticky-navbar a").mPageScroll2id({
         highlightSelector: "#sticky-navbar a",
         liveSelector: "#sticky-navbar a",
