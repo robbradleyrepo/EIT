11c140b48ec7bc915fc829b96a67a785f93fe3e4
Cristina <cristina.dragos@deleteagency.com>
2022-02-16 17:08:07 +0200

    remove image field, the thumbnail to be used instead

--- src/Feature/Listings/website/Models/IAccordionRowModel.cs
+++ src/Feature/Listings/website/Models/IAccordionRowModel.cs
@@ -1,7 +1,6 @@
 ﻿namespace LionTrust.Feature.Listings.Models
 {
     using Glass.Mapper.Sc.Configuration.Attributes;
-    using Glass.Mapper.Sc.Fields;
     using LionTrust.Foundation.Video;
 
     public interface IAccordionRowModel: IVideoModel
@@ -10,9 +9,6 @@
         string Header { get; set; }
 
         [SitecoreField(Listings.Constants.Accordion.CopyField)]
-        string Copy { get; set; }
-
-        [SitecoreField(Listings.Constants.Accordion.ImageField)]
-        Image Image { get; set; }        
+        string Copy { get; set; }        
     }
 }
