6bbae36eb02480fd163cbc51eb36f6954e32afc9
Cristina <cristina.dragos@deleteagency.com>
2022-03-15 11:50:47 +0200

    filter funds in the accordion list by country

--- src/Feature/MyPreferences/website/Controllers/FundAccordionListController.cs
+++ src/Feature/MyPreferences/website/Controllers/FundAccordionListController.cs
@@ -12,18 +12,25 @@
     using System.Linq;
     using System.Collections.Generic;
     using LionTrust.Foundation.Contact.Models;
+    using LionTrust.Foundation.Onboarding.Helpers;
+    using LionTrust.Foundation.Onboarding.Models;
+    using LionTrust.Foundation.Search.Models.Request;
+    using LionTrust.Foundation.Search.Models.ContentSearch;
+    using LionTrust.Foundation.Search.Services.Interfaces;
 
     public class FundAccordionListController : SitecoreController
     {
         private readonly IMvcContext _context;
         private readonly EmailPreferencesService _emailPreferencesService;
         private readonly IPersonalizedContentService _personalizedContentService;
+        private readonly IFundContentSearchService _fundContentSearchService;
 
-        public FundAccordionListController(IMvcContext context, BaseLog log, IMailManager mailManager, IEmailPreferencesRepository editEmailPreferencesRepository, IPersonalizedContentService personalizedContentService)
+        public FundAccordionListController(IMvcContext context, BaseLog log, IMailManager mailManager, IEmailPreferencesRepository editEmailPreferencesRepository, IPersonalizedContentService personalizedContentService, IFundContentSearchService fundContentSearchService)
         {
             _context = context;
             _personalizedContentService = personalizedContentService;
             _emailPreferencesService = new EmailPreferencesService(editEmailPreferencesRepository, mailManager, personalizedContentService);
+            _fundContentSearchService = fundContentSearchService;
         }
 
         public ActionResult Render()
@@ -43,8 +50,22 @@
 
         private IList<SFProcess> GetSPProcessList(Context context)
         {
+            ContentSearchResults<FundSearchResultItem> fundSearchResults = _fundContentSearchService.GetAllAllowedFunds();
+            var allowedFunds = fundSearchResults?.SearchResults?.Select(x => x.Document.SalesforceFundId)?.Where(x => !string.IsNullOrEmpty(x))?.ToList();
+
             var SPProcessList = _emailPreferencesService.GetSFProcessList()?.ToList();
 
+            if (SPProcessList != null && SPProcessList.Any())
+            {
+                foreach (var sfProcess in SPProcessList)
+                {
+                    if (sfProcess.SFFundList != null && sfProcess.SFFundList.Any())
+                    {
+                        var allowedFundList = sfProcess.SFFundList.Where(s => allowedFunds.Contains(s.SFFundId));
+                        sfProcess.SFFundList = allowedFundList?.ToList();
+                    }
+                }
+            }
 
             if (context == null || context.Preferences == null || context.Preferences.SFProcessList == null)
             {
