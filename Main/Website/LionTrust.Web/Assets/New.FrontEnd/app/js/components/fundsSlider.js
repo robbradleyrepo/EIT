2910cb7bbb26e1ca417941818010012175d90e6c
Martha Taylor <martha.taylor@sagittarius.agency>
2022-07-06 10:20:13 +0100

    arrow and scrollbar added

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/fundsSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/fundsSlider.js
@@ -1,12 +1,36 @@
-import Swiper from "swiper/bundle";
+  import Swiper, { Pagination } from "swiper/bundle";
 
-export default () => {
-  const swiper = new Swiper(".swiper-container-funds", {
-    grabCursor: true,
-    slidesPerView: "auto",
-    slidesOffsetAfter: 600,
-    breakpointsInverse: true,
-    loop: true,
+  export default () => {
+    const swiper = new Swiper(".swiper-container-funds", {
+      grabCursor: true,
+      slidesPerView: "auto",
+      slidesOffsetAfter: 600,
+      breakpointsInverse: true,
+      loop: true,
+      observer: true,
+      observeParents: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
 
-  });
-};
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      lazyLoading: true,
+      slideToClickedSlide: true,
+      initialSlide: 1,
+
+      });
+  };
+
+
+
+
+
+  
