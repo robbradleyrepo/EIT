78ca6f85e33a33569f0481abacb2e1ebb0589971
Andre <andre.nascimento@sagittarius.agency>
2022-07-18 17:12:36 +0100

    avoid 500 error on loading page when datasource no longer exist

---
+++ src/Foundation/SitecoreExtensions/website/Pipelines/RenderRendering/ClearInvalidDatasource.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+
+namespace LionTrust.Foundation.SitecoreExtensions.Pipelines.RenderRendering
+{
+    public class ClearInvalidDatasource : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            var rendering = args.Rendering;
+
+            if (!string.IsNullOrWhiteSpace(rendering.DataSource) && Sitecore.Context.Database.Items.GetItem(rendering.DataSource) == null)
+            {
+                rendering.DataSource = string.Empty;
+            }
+        }
+    }
+}
