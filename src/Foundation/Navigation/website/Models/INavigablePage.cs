6afb2c1b150db80498ce2f4fa7a7891aff954a45
cristinadept <cristina.dragos@deptagency.com>
2022-08-03 11:09:33 +0300

    implement the possibility for the second level menu to be set up manually

--- src/Foundation/Navigation/website/Models/INavigablePage.cs
+++ src/Foundation/Navigation/website/Models/INavigablePage.cs
@@ -1,7 +1,7 @@
 ﻿namespace LionTrust.Foundation.Navigation.Models
 {
     using System.Collections.Generic;
-
+    using Glass.Mapper.Sc.Configuration;
     using Glass.Mapper.Sc.Configuration.Attributes;
     using LionTrust.Foundation.Legacy.Models;
     using LionTrust.Foundation.ORM.Models;
@@ -10,5 +10,8 @@
     {
         [SitecoreChildren]
         IEnumerable<INavigablePage> Children { get; set; }
+
+        [SitecoreField(Constants.HeaderConfiguration.MenuItems_FieldID, SitecoreFieldType.Treelist, "Menu")]
+        IEnumerable<INavigablePage> MenuItems { get; set; }
     }
 }
