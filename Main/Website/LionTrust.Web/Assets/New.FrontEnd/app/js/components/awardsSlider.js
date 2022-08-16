05b911c1a8e87deffcbe1a5a5550064ec40de877
Martha Taylor <martha.taylor@sagittarius.agency>
2022-08-16 10:01:32 +0100

    center awards if less then or equal to three and hide pagination features

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/awardsSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/awardsSlider.js
@@ -1,60 +1,71 @@
-    import Swiper from 'swiper/bundle';
+import Swiper from 'swiper/bundle';
 
-    // core version + navigation, pagination modules:
-    import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+// core version + navigation, pagination modules:
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 
-    // configure Swiper to use modules
-    SwiperCore.use([Navigation, Pagination]);
-    
+// configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination]);
 
-    export default () => {
-        const countAllSlides = document.querySelectorAll('.mainSlideDiv').length;
-        const threeSliders = 3;
 
-        if(countAllSlides <= threeSliders) {
-            document.getElementsByClassName('swiper-wrapper awardsWrapper')[0].classList.add('awardscentered');
-            document.getElementsByClassName('swiper-pagination awardspag')[0].classList.add('hidePagination');
+export default () => {
+
+    let nodeList = document.querySelectorAll(".swiper-container-awards");
+    for (let i = 0; i < nodeList.length; i++) {
+     
+
+        let countAllSlides = nodeList[i].querySelectorAll('.slideDiv').length;
+        console.log(countAllSlides)
+        let threeSliders = 3;
+
+        if (countAllSlides <= threeSliders) {
+            console.log("hi")
+            nodeList[i].getElementsByClassName('swiper-wrapper slidesWrapper')[0].classList.add('centerawards');
+            nodeList[i].getElementsByClassName('swiper-pagination awardspagination')[0].classList.add('hideawardsPagination');
         }
-        
-        new Swiper('.swiper-container-awards', {
 
-            grabCursor: true,
-            slidesPerView: 3,
-            centeredSlides: true,
-            spaceBetween: 32,
-            speed: 800,
-            loop: false,
-            simulateTouch : true,
-            loopFillGroupWithBlank: false,
-            breakpointsInverse: true,
-            breakpoints: {
-                0: {
-                    slidesPerView: 1.8,
-                    centeredSlides: false
-                },
-                576: {
-                    slidesPerView: 2,
-                    centeredSlides: true
-                }, 
-                768: {
-                    slidesPerView: 2.2,
-                    centeredSlides: false
-                },
-                992: {
-                    slidesPerView: 3,
-                    centeredSlides: false
-                },            
-            },
-
-
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            }
-        });
     }
+
+
+    new Swiper('.swiper-container-awards', {
+
+        grabCursor: true,
+        slidesPerView: 3,
+        centeredSlides: true,
+        spaceBetween: 32,
+        speed: 800,
+        loop: false,
+        simulateTouch: true,
+        loopFillGroupWithBlank: false,
+        breakpointsInverse: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 1.8,
+                centeredSlides: false
+            },
+            576: {
+                slidesPerView: 2,
+                centeredSlides: true
+            },
+            768: {
+                slidesPerView: 2.2,
+                centeredSlides: false
+            },
+            992: {
+                slidesPerView: 3,
+                centeredSlides: false
+            },
+        },
+
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    });
+  
+}
