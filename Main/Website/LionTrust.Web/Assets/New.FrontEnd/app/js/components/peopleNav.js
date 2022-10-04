ad97e782d99fdb7ce7e3dc27b903d72a83f2ffab
Nicolas Bouffault <nicolas.bouffault@sagittarius.agency>
2022-10-04 19:52:21 +0100

    LB-806 - Tabs people mobile part 1

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/peopleNav.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/peopleNav.js
@@ -1,15 +1,23 @@
 export default () => {
+    //active class
     var navLinks = $(".page-anchor__links li a");
     var pathname = location.pathname.split(".");
     pathname.pop();
     
     var pathnameTxt = pathname.join();
     
-
     navLinks.each(function(){
         if($(this).attr("href").indexOf(pathnameTxt) !== -1) {
             $(this).addClass("active")
         }
     })
 
+    //toggle nav mobile
+    var selectBtn = $(".people-anchors .page-anchor__title");
+    selectBtn.on('click', function(){
+        if(window.innerWidth < 992) {
+            $(this).toggleClass("active");
+            $(this).next().toggleClass("open");
+        }
+    });
 }
