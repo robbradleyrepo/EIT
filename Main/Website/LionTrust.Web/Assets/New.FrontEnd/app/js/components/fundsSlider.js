2834a1379d2ba87936595cbcf0cac72cc04cd561
sevalbekir <seval.bekir@sagittarius.agency>
2022-10-13 08:45:17 +0100

    LB-924 fix fundscroller bugs, solve merge conflict

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/fundsSlider.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/fundsSlider.js
@@ -1,39 +1,48 @@
-  import Swiper, { Pagination } from "swiper/bundle";
+import Swiper, { Pagination } from "swiper/bundle";
 
-  export default () => {
-    const swiper = new Swiper(".swiper-container-funds", {
-      grabCursor: true,
-      slidesPerView: "auto",
-      breakpointsInverse: true,
-      slidesOffsetAfter: 600,
-      loop: true,
-      observer: true,
-      observeParents: true,
-      watchSlidesVisibility: true,
-      watchSlidesProgress: true,
+export default () => {
+  const swiper = new Swiper(".swiper-container-funds", {
+    grabCursor: true,
+    slidesPerView: "auto",
+    breakpointsInverse: true,
+    slidesOffsetAfter: 600,
+    loop: true,
+    observer: true,
+    observeParents: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
 
-      pagination: {
-      el: '.swiper-pagination',
+    pagination: {
+      el: ".swiper-pagination",
       clickable: true,
-      renderBullet: function(index, className) {
+      renderBullet: function (index, className) {
         return `<span class="dot swiper-pagination-bullet"></span>`;
       },
-      },  
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      hidePaginatiom: function () {
+        if (this.slidesPerView.length > 3) {
+          prevArrow.classList.add("hide-arrows");
+          nextArrow.classList.add("hide-arrows");
+        }
       },
+    },
 
-      lazyLoading: true,
-      slideToClickedSlide: true,
-      initialSlide: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-      });
-  };
+    lazyLoading: true,
+    slideToClickedSlide: true,
+    initialSlide: 1,
+  });
 
-
-
-
-
+  let description = document.getElementsByClassName("imagepromo__contentbox");
+  let fundScroller = document.getElementsByClassName("fundscroller-container");
+  let slides = document.getElementsByClassName("swiper-slides");
+  let prevArrow = document.getElementsByClassName("swiper-button-prev");
+  let nextArrow = document.getElementsByClassName("swiper-button-next");
+  if (description.length === 0) {
+    fundScroller.classList.add("full-width-carousel");
+  }
   
+};
