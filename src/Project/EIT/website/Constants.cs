3db94682a0b292a53cab99a00c6628f47b1746f4
Andre <andre.nascimento@sagittarius.agency>
2022-11-01 13:55:24 +0000

    creating EIT project and update configuration

---
+++ src/Project/EIT/website/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace LionTrust.Project.EIT
+{
+    public static class Constants
+    {
+        public static class PageTypes
+        {
+            public const string HomeTemplateId = "{51D3A578-6E10-4EC6-BB5B-5C1307A6D661}";
+
+            public const string SearchPageTemplateId = "{86212646-0646-4F63-B4DB-6C3F247989B1}";
+        }
+
+        public static class Renderings
+        {
+            public const string FundCentreRenderingId = "{FCEF6713-BC3D-47F1-9178-7526D6453DBC}";
+
+            public const string MediaGalleryRenderingId = "{BC23D3DF-6124-4437-80F5-D53E3F24113C}";
+
+            public const string IFrameModuleRenderingId = "{C8A0835C-41F7-4918-B665-F5476CFB3ADE}";
+
+            public const string IFrameCASModuleRenderingId = "{D5A4F604-2332-4ACD-8B59-CD26BC0555C9}";
+        }
+    }
+}
