c01ff5e12c12035ee1b7f6c3ce62fc0eddcd089b
Andre <andre.nascimento@sagittarius.agency>
2022-11-08 17:43:31 +0000

    fix publish issue

---
+++ src/Foundation/SitecoreExtensions/website/Constants.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Data;
+
+namespace LionTrust.Foundation.SitecoreExtensions
+{
+    public static class Constants
+    {
+        public static class Site
+        {
+            public static ID Template_ID = new ID("{BB85C5C2-9F87-48CE-8012-AF67CF4F765D}");
+        }
+    }
+}
