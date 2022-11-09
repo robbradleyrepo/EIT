22d4c5769235e9a7aad7e83b6e9383c3cf318a3a
sevalbekir <seval.bekir@sagittarius.agency>
2022-11-09 21:50:32 +0000

    LB-896 Update Insight author section and create a separate js file for it

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/common.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/common.js
@@ -2,20 +2,10 @@ export default () => {
 $('#CybotCookiebotDialogPoweredbyImage').attr('src','/-/media/cookie-logo');
   var avatarLength = $('.hero-manager-link__link').length;
   
-  let numberOfAuthors = $(".hero-manager-link__link").length;
-  let authors = $(".hero-manager-link__link");
-  let maxNumberAuthors = 4;
-
 if(avatarLength > 4)
 {
   $('.hero-manager-link').addClass('avatarLinks-row');
   $('.hero-manager-link__link:nth-child(-n+4)').wrapAll('<div class="avatar-row"></div>');
   $('.hero-manager-link > .hero-manager-link__link').wrapAll('<div class="avatar-row"></div>');
-}
-
- if (numberOfAuthors >= maxNumberAuthors) {
-        authors.hide();
-       
-   }  
-  
+}  
 };
