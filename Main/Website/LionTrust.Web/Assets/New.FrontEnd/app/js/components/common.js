3322128a2aac6c7358cd44d120c631f992c9485e
sevalbekir <seval.bekir@sagittarius.agency>
2022-11-09 17:00:49 +0000

    LB-896 Update Insight Author section

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/common.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/common.js
@@ -1,6 +1,11 @@
 export default () => {
 $('#CybotCookiebotDialogPoweredbyImage').attr('src','/-/media/cookie-logo');
-var avatarLength = $('.hero-manager-link__link').length;
+  var avatarLength = $('.hero-manager-link__link').length;
+  
+  let numberOfAuthors = $(".hero-manager-link__link").length;
+  let authors = $(".hero-manager-link__link");
+  let maxNumberAuthors = 4;
+
 if(avatarLength > 4)
 {
   $('.hero-manager-link').addClass('avatarLinks-row');
@@ -8,4 +13,9 @@ if(avatarLength > 4)
   $('.hero-manager-link > .hero-manager-link__link').wrapAll('<div class="avatar-row"></div>');
 }
 
+ if (numberOfAuthors >= maxNumberAuthors) {
+        authors.hide();
+       
+   }  
+  
 };
