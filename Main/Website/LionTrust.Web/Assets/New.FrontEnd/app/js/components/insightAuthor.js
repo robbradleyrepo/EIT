725a109f64ca5c5d0b952fdc2b7edcfd15b06942
sevalbekir <seval.bekir@sagittarius.agency>
2022-11-10 09:42:09 +0000

    LB-896 Use existing variable instead of numberOfAuthors

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/insightAuthor.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/app/js/components/insightAuthor.js
@@ -1,10 +1,10 @@
 export default () => {
  
-  let numberOfAuthors = $(".hero-manager-link__link").length;
+  let avatarLength = $('.hero-manager-link__link').length;
   let authors = $(".hero-manager-link__link");
   let maxNumberAuthors = 4;
   
-   if (numberOfAuthors >= maxNumberAuthors) {
+   if (avatarLength >= maxNumberAuthors) {
         authors.hide();
        
    }
