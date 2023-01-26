30ea011b9aca1627f4b3816523bfdd2a46caed5e
Rob Bradley <paraxia@hotmail.com>
2023-01-26 11:43:38 +0000

    Process cards

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/slide-panel.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/slide-panel.js
@@ -9,39 +9,54 @@ export default () => {
             this.$componentSelector = $theComponentSelector;
             this.$body = $('body');
             this.$navScreen = $('.u-screen'); 
-            this.$navMasterControl = this.$body.find('.f-slide-panel');
-            this.$navClose = $('.g-slide-panel__close');   
+            this.$navMasterControl = $('.f-slide-panel'); 
+            this.$navClose = $('.g-slide-panel__close');  
+            this.$panelLinks = this.$componentSelector.find('ul > li > a');
             this.initSlidePanelComponent();
         }
         SlidePanelComponent.prototype.initSlidePanelComponent = function () {
             this.slidePanelToggle(); 
+            this.refsSlidePanel()
         }; 
         SlidePanelComponent.prototype.openSlidePanel = function () { 
-            var _self = this; 
             this.$componentSelector.addClass('active').attr('aria-hidden', 'false');
             this.$navScreen.addClass('active');
-            _self.$body.addClass('overflow-hidden'); 
+            this.$body.addClass('overflow-hidden');  
         }; 
         SlidePanelComponent.prototype.closeSlidePanel = function () {
             var _self = this; 
+
             this.$componentSelector.removeClass('active').attr('aria-hidden', 'true');
-            this.$navScreen.removeClass('active'); 
-            _self.$body.removeClass('overflow-hidden');
+            this.$navScreen.removeClass('active');  
+            setTimeout(function () {      
+                _self.$body.removeClass('overflow-hidden');
+            }, 300);
             
         }; 
         SlidePanelComponent.prototype.slidePanelToggle = function () {
             var _self = this; 
             _self.$navMasterControl.on('click', function (e) { 
-                e.preventDefault(); 
+                e.preventDefault();  
                 _self.openSlidePanel(); 
                 
             });
-            // close nav
             _self.$navClose.on('click', function (e) {
                 e.preventDefault(); 
                 _self.closeSlidePanel();
-                
             });
+            _self.$navScreen.on('click', function (e) {
+                e.preventDefault(); 
+                _self.closeSlidePanel();
+            });
+        };
+        SlidePanelComponent.prototype.refsSlidePanel = function () {
+            var _self = this; 
+            _self.$panelLinks.on('click', function (e) { 
+                e.preventDefault(); 
+            }); 
+            if (window.location.hash.indexOf('activepanel') != -1) {
+                _self.openSlidePanel(); 
+            } 
         };
         return SlidePanelComponent;
     }());
