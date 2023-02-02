8df8fe2c541ef0b8fff4ad198bf9311dc0cec2bf
Rob Bradley <paraxia@hotmail.com>
2023-02-02 17:46:38 +0000

    Breakpoint for Intersection Observer. Only kicks in above 992px vw

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/scrolldetect.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/scrolldetect.js
@@ -2,43 +2,57 @@
 var $ = require("jquery");
 export default () => {
 
+
 	const images = document.querySelectorAll('.js-lazyload-image');
 	const sections = document.querySelectorAll('.t-global main section.u-edge:not(:first-child), .t-global main aside.u-edge:not(:first-child), .t-global main section.u-edge.c-stats-banner');
 
-	let config = {
-		rootMargin: '100px',
-		threshold: 0.6,
-		triggerOnce: true,
-	};
+	function widthResizer(){
+		var width = window.innerWidth
+		if (width > 992) {
+			engageObserver(); 
+		}  
 
-	let observer = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			intersectionHandler(entry);
-		} 
+	}
+	  
+	function engageObserver() {
+		let config = {
+			rootMargin: '100px',
+			threshold: 0.6,
+			triggerOnce: true,
+		};
 
+		let observer = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				intersectionHandler(entry);
+			} 
+
+			});
+		}, config);
+
+		sections.forEach(section => {
+			observer.observe(section);
 		});
-	}, config);
 
-	sections.forEach(section => {
-		observer.observe(section);
-	});
+		function intersectionHandler(entry) {
+			const next = entry.target; 
+			if (next) {
+				next.classList.add('active'); 
+			}
+		}
 
-	function intersectionHandler(entry) {
-		const next = entry.target; 
-		if (next) {
-			next.classList.add('active'); 
+		images.forEach(image => {
+			observer.observe(image);
+		});
+
+		function preloadImage(img) {
+			const src = img.getAttribute('data-src');
+			if (!src) { return; }
+			img.src = src;
 		}
 	}
 
-	images.forEach(image => {
-		observer.observe(image);
-	});
-
-	function preloadImage(img) {
-		const src = img.getAttribute('data-src');
-		if (!src) { return; }
-		img.src = src;
-	}
+	widthResizer();
+	window.addEventListener('resize', widthResizer);
 
 }
