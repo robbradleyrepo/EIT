af48b56c2979ea43062ab7114a747d5e39ae866d
Rob Bradley <paraxia@hotmail.com>
2023-02-09 17:02:39 +0000

    EC053 Transcript FE built

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/accordion.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/accordion.js
@@ -6,8 +6,26 @@ export default () => {
     var AccordionComponent = /** @class */ (function () {
 
         function AccordionComponent($theComponentSelector) {
-            $theComponentSelector.beefup();
-        }
+            this.$componentSelector = $theComponentSelector;   
+            this.$closeControl = this.$componentSelector.find('.beefup-close');  
+            
+            this.$componentSelector.beefup({
+                scrollOffset: -100
+            }); 
+
+            this.closeAccordionComponent();
+        };
+
+        AccordionComponent.prototype.closeAccordionComponent = function () {
+            var _self = this; 
+            _self.$closeControl.on('click', function (e) {
+                e.preventDefault(); 
+             
+                _self.$componentSelector.close($(_self.$componentSelector));
+                _self.$componentSelector.scroll($(_self.$componentSelector));
+            }); 
+            
+        };
       
         return AccordionComponent;
     }());
