5b7c554ea79d4f27f41bbb2c9c097a7ca76cdd94
Rob Bradley <paraxia@hotmail.com>
2023-02-10 13:14:43 +0000

    Share Link refactored, changed to clickable div, a tag causing page jump issues

--- Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/share-link.js
+++ Main/Website/LionTrust.Web/Assets/New.FrontEnd/EIT/js/edge/share-link.js
@@ -6,13 +6,10 @@ export default () => {
     var ShareLinkComponent = /** @class */ (function () {
 
         function ShareLinkComponent($theComponentSelector) {
-            this.$componentSelector = $theComponentSelector;   
-            this.$control = this.$componentSelector.find('.f-share-link');  
+            this.$control = $theComponentSelector;
             this.clickShareLinkComponent(); 
         }
-          
-       
-        // event listeners
+           
         ShareLinkComponent.prototype.clickShareLinkComponent = function () {
             var _self = this;
             _self.$control.on('click', function (e) {
@@ -20,9 +17,9 @@ export default () => {
                 const self = $(this);
                 const copyLink = self.data("link");
                 copy(copyLink);
-                self.addClass("active");
+                self.addClass("triggered");
                 setTimeout(function() { 
-                    self.removeClass('active');
+                    self.removeClass('triggered');
                 }, 4000);
             });  
         };
@@ -31,7 +28,7 @@ export default () => {
     }());
 
     $(function () {
-        var shareLinkComponentHolder = '.u-edge';
+        var shareLinkComponentHolder = '.f-share-link';
         $(shareLinkComponentHolder).each(function () {
             var shareLinkComponent = new ShareLinkComponent($(this));
         });
